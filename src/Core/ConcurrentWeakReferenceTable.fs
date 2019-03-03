fa27da0ab8c8a617ac0a4047314604e7251aa62a
Minkyu Jung <hestati@kaist.ac.kr>
2019-03-03 16:10:14 +0900

    [Core] Optimize ConcurrentWeakReferenceTable

--- src/Core/ConcurrentWeakReferenceTable.fs
+++ src/Core/ConcurrentWeakReferenceTable.fs
@@ -28,30 +28,38 @@ namespace B2R2
 
 open System
 open System.Threading
+open System.Runtime.InteropServices
 
-type private Node<'T> =
-  {
-    Value: 'T
-    Next: Node<'T>
-  }
+type private Node<'T> = {
+  Value         : 'T
+  Hash          : int
+  mutable Next  : Node<'T>
+}
 
 [<AutoOpen>]
 module private ConcurrentWeakReferenceTableHelper =
   let inline (===) e1 e2 =
     LanguagePrimitives.PhysicalEquality e1 e2
 
+  let inline (!==) e1 e2 =
+    LanguagePrimitives.PhysicalEquality e1 e2 |> not
+
+  let rec nextIntegerNotDivisibleBy357 sz =
+    if sz % 3 = 0 || sz % 5 = 0 || sz % 7 = 0 then
+      nextIntegerNotDivisibleBy357 (sz + 2)
+    else sz
+
   let inline nextSize currentSize =
-    let rec nextIntegerNotDivisibleBy357 sz =
-      if sz % 3 = 0 || sz % 5 = 0 || sz % 7 = 0 then
-        nextIntegerNotDivisibleBy357 (sz + 2)
-      else sz
     nextIntegerNotDivisibleBy357 <| currentSize * 2
 
-  let createEmptyNodeRef _ = ref Unchecked.defaultof<_>
-  let createEmptyLockRef _ = ref (Object())
+  let createEmptyNodeRef _ = Unchecked.defaultof<_>
 
-  let getBucketNo hashcode _buckets =
-      (hashcode &&& 0x7fffffff) % (Array.length _buckets)
+  let createEmptyLockRef _ = new Object ()
+
+  let inline getBucketNo hc _buckets =
+      (hc &&& 0x7fffffff) % (Array.length _buckets)
+
+  let concurrency = Environment.ProcessorCount
 
 /// <summary>
 ///   Weak-reference table that supports concurrency.
@@ -59,114 +67,116 @@ module private ConcurrentWeakReferenceTableHelper =
 type ConcurrentWeakReferenceTable<'T when 'T : equality and 'T : not struct>() =
   let mutable capacity = 31
   let mutable maxPerBucket = 251
-  let concurrency = Environment.ProcessorCount
   let mutable buckets = Array.init capacity createEmptyNodeRef
   let locks = Array.init concurrency createEmptyLockRef
 
-  member private __.GetBucketNoAndLockNo hashcode _buckets =
-    let bucketNo = getBucketNo hashcode _buckets
-    bucketNo, bucketNo % concurrency
-
-  member private __.GetValue x hashcode =
+  member private __.TryGetValue (x, hc, [<Out>] res : byref<'T>) =
     let _buckets = buckets
-    let bucketNo = getBucketNo hashcode _buckets
-    let mutable res = None
-    let v = ref (Unchecked.defaultof<_>)
-    let mutable t = Volatile.Read<Node<WeakReference<'T>>>(_buckets.[bucketNo])
-    while not (t === Unchecked.defaultof<_>) do
-      let h = t.Value
-      if h.TryGetTarget(v) then
-        let v = (!v)
-        if v.Equals(x) then res <- Some v; t <- Unchecked.defaultof<_>
-                       else t <- t.Next
-      else
-        t <- t.Next
-    res
+    let bucketNo = getBucketNo hc _buckets
+    let mutable t = Volatile.Read<Node<WeakReference<'T>>>(&_buckets.[bucketNo])
+    let mutable found = false
+    while t !== Unchecked.defaultof<_> do
+      if hc = t.Hash then (* Fast Check *)
+        match t.Value.TryGetTarget () with
+        | true, v when v.Equals x ->
+          res <- v; t <- Unchecked.defaultof<_>; found <- true
+        | _ -> t <- t.Next
+      else t <- t.Next
+    found
 
   member private __.AcquireLocks st ed =
     let mutable acquired = 0
-    for idx in st .. (ed - 1) do
+    let mutable idx = st
+    while idx < ed do
       let lockTaken = ref false
-      try Monitor.Enter(locks.[idx], lockTaken)
-      finally ()
-      if (!lockTaken) then
-        acquired <- acquired + 1
+      Monitor.Enter (locks.[idx], lockTaken)
+      if !lockTaken then acquired <- acquired + 1
+      idx <- idx + 1
     acquired
 
   member private __.ReleaseLocks st ed =
-    for idx in st .. (ed - 1) do Monitor.Exit (locks.[idx])
+    let mutable idx = st
+    while idx < ed do Monitor.Exit locks.[idx]; idx <- idx + 1
 
   member private __.Resize () =
     let _buckets = buckets
-    let mutable locksAcquired = 0
-    try
-      (* First of All acquire Lock0 to resize *)
-      locksAcquired <- locksAcquired + __.AcquireLocks 0 1
-      (* Confirm any other thread does resize *)
-      if _buckets === buckets then
-        let nsize = nextSize capacity
-        let nbuckets = Array.init nsize createEmptyNodeRef
-        for lst in buckets do
-          let mutable t = (!lst)
-          while not (t === Unchecked.defaultof<_>) do
-            let elem = t.Value
-            let v = ref (Unchecked.defaultof<_>)
-            if elem.TryGetTarget(v) then
-              let no = getBucketNo ((!v).GetHashCode()) nbuckets
-              Volatile.Write(nbuckets.[no], { Value = elem
-                                              Next = !nbuckets.[no] })
-            t <- t.Next
-        (* Acquire other Locks *)
-        locksAcquired <- locksAcquired + __.AcquireLocks 1 locks.Length
-        capacity <- nsize
-        buckets <- nbuckets
-    finally
-      __.ReleaseLocks 0 locksAcquired
+    (* First of All acquire Lock0 to resize *)
+    let mutable locksAcquired = __.AcquireLocks 0 1
+    (* Confirm that any other thread does not call `resize ()` *)
+    if _buckets === buckets then
+      let nsize = nextSize capacity
+      let nbuckets = Array.init nsize createEmptyNodeRef
+      for head in buckets do
+        let mutable t = head
+        while t !== Unchecked.defaultof<_> do
+          match t.Value.TryGetTarget () with
+          | true, _ ->
+            let no = getBucketNo t.Hash nbuckets
+            (* Create new node and link *)
+            Volatile.Write<Node<WeakReference<'T>>>
+              (&nbuckets.[no], { t with Next = nbuckets.[no] })
+          | false, _ -> ()
+          t <- t.Next
+      (* Acquire other Locks *)
+      locksAcquired <- locksAcquired + __.AcquireLocks 1 locks.Length
+      capacity <- nsize
+      buckets <- nbuckets
+    __.ReleaseLocks 0 locksAcquired
 
-  member private __.AddValueInternal x hashcode valueFactory =
-    let _buckets = buckets
-    let (bucketNo, lockNo) = __.GetBucketNoAndLockNo hashcode _buckets
-    let lockTaken = ref false
-    try
-      Monitor.Enter(locks.[lockNo], lockTaken)
+  member private __.AddValueInternal (x, hc, factory, [<Out>] sz: byref<int>) =
+      let _buckets = buckets
+      let bucketNo = getBucketNo hc _buckets
+      let lockNo = bucketNo % concurrency
+      let lockTaken = ref false
+      let mutable hd = Unchecked.defaultof<_>
+      Monitor.Enter (locks.[lockNo], lockTaken)
       if buckets === _buckets then
-        let mutable t = (!_buckets.[bucketNo])
-        let mutable res = None
-        let mutable size = 0
-        let mutable out = Unchecked.defaultof<_>
+        let mutable t = _buckets.[bucketNo]
+        let mutable found = false
+        let mutable res = Unchecked.defaultof<_>
+        let mutable prev = Unchecked.defaultof<_>
+        sz <- 0
         (* XXX: We assume that no duplicate entry exists *)
-        while not (t === Unchecked.defaultof<_>) do
-          let h = t.Value
-          let v = ref Unchecked.defaultof<_>
-          match h.TryGetTarget(v) with
-          | true ->
-            let v = (!v)
-            if v.Equals(x) then res <- Some v; t <- Unchecked.defaultof<_>
-            else
-              out <- { Value = h; Next = out }
-              t <- t.Next; size <- size + 1
-          | false -> t <- t.Next (* Erase GC-ed cell *)
-        match res with
-        | Some v -> v, 0
-        | None ->
-          let newValue = valueFactory x
-          let newValueCell = WeakReference<'T>(newValue)
-          Volatile.Write(_buckets.[bucketNo], { Value = newValueCell
-                                                Next = out })
-          newValue, size
-      else __.AddValueInternal x hashcode valueFactory
-    finally
-      if (!lockTaken) then Monitor.Exit(locks.[lockNo]) |> ignore
+        while t !== Unchecked.defaultof<_> do
+          match t.Value.TryGetTarget () with
+          | true, v ->
+            (* Set first none-empty cell as head *)
+            if hd === Unchecked.defaultof<_> then hd <- t
+            prev <- t
+            if v.Equals x then
+              res <- v; found <- true; t <- Unchecked.defaultof<_>
+            else t <- t.Next; sz <- sz + 1
+          | false, _ -> (* Erase GC-ed Cell *)
+            if prev !== Unchecked.defaultof<_> then (* Head is dead *)
+              Volatile.Write<Node<WeakReference<'T>>> (&prev.Next, t.Next)
+            t <- t.Next
+        if found then (* Insertion from other thread while waiting lock *)
+          Volatile.Write<Node<WeakReference<'T>>> (&_buckets.[bucketNo], hd)
+          if !lockTaken then Monitor.Exit (locks.[lockNo]) |> ignore
+          sz <- 0; res
+        else
+          let hd = Volatile.Read<Node<WeakReference<'T>>>(&_buckets.[bucketNo])
+          let nV = factory x
+          let nVCell = WeakReference<'T> (nV)
+          Volatile.Write<Node<WeakReference<'T>>>
+            (&_buckets.[bucketNo], { Value = nVCell
+                                     Hash  = hc
+                                     Next  = hd })
+          if !lockTaken then Monitor.Exit (locks.[lockNo]) |> ignore
+          nV
+      else
+        if !lockTaken then Monitor.Exit (locks.[lockNo]) |> ignore
+        __.AddValueInternal (x, hc, factory, &sz)
 
-  member private __.AddValue x hashcode valueFactory =
-    let (newValue, sz) = __.AddValueInternal x hashcode valueFactory
-    if sz > maxPerBucket then __.Resize()
-    newValue
+  member private __.AddValue x hc factory =
+    let nV, sz = __.AddValueInternal (x, hc, factory)
+    if sz > maxPerBucket then __.Resize ()
+    nV
 
-  member __.GetOrApplyAndAdd x valueFactory =
-    let hashCode = x.GetHashCode()
-    match __.GetValue x hashCode with
-    | Some value -> value
-    | None -> __.AddValue x hashCode valueFactory
+  member __.GetOrApplyAndAdd x factory =
+    let hc = x.GetHashCode ()
+    match __.TryGetValue (x, hc) with
+    | true, v -> v
+    | _ -> __.AddValue x hc factory
 
   member __.GetOrAdd x = __.GetOrApplyAndAdd x id
