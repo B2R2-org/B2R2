980061cd7762aadfbe9231eb8daaffb078f9f39e
Dongkwan Kim <0xdkay@gmail.com>
2019-05-14 22:48:16 +0900

    [Core] Fix bug in BitVector.sdiv
    BitVector.sdiv negates input bitvectors only if they have contrary
    signs. However, for negative values, it gets false result because
    BitVector.binOp internally use unsigned integer. Fixing this by firstly
    making values positive and then negates the result value if necessary.

--- src/Core/BitVector.fs
+++ src/Core/BitVector.fs
@@ -429,13 +429,16 @@ type BitVector =
   [<CompiledName("Div")>]
   static member div v1 v2 = BitVector.binOp (/) (bigint.Divide) v1 v2
 
+  static member signedOp v1 v2 op =
+    let sign1, sign2 = BitVector.isPositive v1, BitVector.isPositive v2
+    let bv1 = if sign1 then v1 else BitVector.neg v1
+    let bv2 = if sign2 then v2 else BitVector.neg v2
+    let bv = op bv1 bv2
+    if sign1 = sign2 then bv
+    else BitVector.neg bv
+
   [<CompiledName("Sdiv")>]
-  static member sdiv v1 v2 =
-    if BitVector.isPositive v1 && BitVector.isNegative v2 then
-      BitVector.binOp (/) (bigint.Divide) v1 (BitVector.neg v2) |> BitVector.neg
-    elif BitVector.isNegative v1 && BitVector.isPositive v2 then
-      BitVector.binOp (/) (bigint.Divide) (BitVector.neg v1) v2 |> BitVector.neg
-    else BitVector.binOp (/) (bigint.Divide) v1 v2
+  static member sdiv v1 v2 = BitVector.signedOp v1 v2 BitVector.div
 
   [<CompiledName("Modulo")>]
   static member modulo v1 v2 = BitVector.binOp (%) (bigint.op_Modulus) v1 v2
