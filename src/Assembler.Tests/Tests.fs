220082a38d2ec0db019a2f8dffa3f0a13d459c5c
Kangsu Kim <kskim0610@kaist.ac.kr>
2019-05-23 00:16:52 +0900

    [Asm] Add test boilerplate

---
+++ src/Assembler.Tests/Tests.fs
@@ -0,0 +1,11 @@
+namespace B2R2.Assembler.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
