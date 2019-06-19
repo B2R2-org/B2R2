cdf5062f1d7e303c1f0740941b0b72a57af00e1e
Subin Jeong <cyclon2@kaist.ac.kr>
2019-06-19 10:56:49 +0900

    [WebUI] Add history on web terminal

--- src/Utilities/BinExplorer/WebUI/js/b2r2.terminal.js
+++ src/Utilities/BinExplorer/WebUI/js/b2r2.terminal.js
@@ -1,5 +1,8 @@
+var cmdHistory = [""];
+var idx = 0;
+
 function Terminal() {
-  const webCommand = ["show"]
+  const webCommand = ["show"];
   function setTerminal(width) {
     $(".sidebar-content").css("width", width);
     $(".sidebar-content").css("min-width", width);
@@ -20,6 +23,8 @@ function Terminal() {
     const escaped = $('<div/>').text("B2R2> " + cmd + "\n" + result).html();
     const template = "<div>{CONTENT}</div>".replace("{CONTENT}", escaped);
     $("#id_content").append(template);
+    cmdHistory.push(cmd);
+    idx = 0;
     resetCmdInput();
   }
   function executeCommand(cmd) {
@@ -42,43 +47,70 @@ function Terminal() {
   }
   function onCommandSubmit() {
     var key = window.event.keyCode;
-    if (key === 13) {
-      const cmd = $("#id_cmd").val().trim();
-      const keyword = cmd.split(" ")[0];
-      if (isWebCommand(keyword)) {
-        switch (keyword) {
-          case "show":
-            const funcName = cmd.split(" ")[1];
-            query({
-              "q": "cfg-disasm",
-              "args": funcName
-            },
-              function (status, json) {
-                console.log();
-                if (status != 404 && !isEmpty(json)) {
-                  let dims = reloadUI();
-                  addTab(funcName, dims, json);
-                  drawCFG(dims, json);
-                  UIElementInit(true);
-                  autocomplete(json);
-                  setuiFuncName(funcName);
-                  addCommands(cmd, "");
-                } else {
-                  addCommands(cmd, "[*] Unknown function: '" + funcName + "'");
-                }
-              });
-            break;
-          default:
-            break;
+    switch (key) {
+      case 13:
+        const cmd = $("#id_cmd").val().trim();
+        const keyword = cmd.split(" ")[0];
+        if (isWebCommand(keyword)) {
+          switch (keyword) {
+            case "show":
+              let funcName = cmd.split(" ")[1];
+              if (funcName === undefined) { funcName = ""; }
+              query({
+                "q": "cfg-disasm",
+                "args": funcName
+              },
+                function (status, json) {
+                  if (status != 404 && !isEmpty(json)) {
+                    let dims = reloadUI();
+                    addTab(funcName, dims, json);
+                    drawCFG(dims, json);
+                    UIElementInit(true);
+                    autocomplete(json);
+                    setuiFuncName(funcName);
+                    addCommands(cmd, "");
+                  } else {
+                    addCommands(cmd, "[*] Unknown function: '" + funcName + "'");
+                  }
+                });
+              break;
+            case "clear":
+
+              break;
+            default:
+              break;
+          }
+        } else {
+          // run b2r2 cli
+          executeCommand(cmd);
         }
-      } else {
-        // run b2r2 cli
-        executeCommand(cmd);
-      }
-      return false;
-    }
-    else {
-      return true;
+        break;
+      case 38: //upArrow
+        idx++;
+        if (idx < 0) {
+          idx = -1;
+        } else if (idx > cmdHistory.length - 1) {
+          idx = cmdHistory.length - 1;
+        }
+        $("#id_cmd").val(cmdHistory[cmdHistory.length - idx]);
+        $('#id_cmd').focus().val(cmdHistory[cmdHistory.length - idx]);
+        $('#id_cmd').focus(function () {
+          var that = this;
+          setTimeout(function () { that.selectionStart = that.selectionEnd = 10000; }, 0);
+        });
+        break;
+      case 40: //downArrow
+        idx--;
+        if (idx < 0) {
+          idx = 0;
+        } else if (idx > cmdHistory.length - 1) {
+          idx = cmdHistory.length - 1;
+        }
+        $("#id_cmd").val(cmdHistory[cmdHistory.length - idx]);
+        $('#id_cmd').focus().val(cmdHistory[cmdHistory.length - idx]);
+        break;
+      default:
+        break;
     }
   }
   return {
