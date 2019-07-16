ff82d7e281eca2ba6f6a16c407b310f7cd355889
Subin Jeong <cyclon2@kaist.ac.kr>
2019-07-16 15:04:41 +0900

    [WebUI] Add resizer on sidebar

--- src/Utilities/BinExplorer/WebUI/js/b2r2.sidebar.js
+++ src/Utilities/BinExplorer/WebUI/js/b2r2.sidebar.js
@@ -42,7 +42,8 @@ function Sidebar() {
 
   $(document).on("click", ".sidebar-item", function () {
     function show(wrapper) {
-      $(".sidebar-content > div").hide();
+      $(".sidebar-content > .sidebar-content-item").hide();
+      $(".sidebar-content > .sidebar-content-wide-item").hide();
       Terminal().deleteTerminal();
       $(wrapper).show();
     }
@@ -54,7 +55,7 @@ function Sidebar() {
         break
       case "Comments":
         show("#id_CommentsListWrapper");
-        setSidebarComments();
+        Sidebar().setSidebarComments();
         break;
       case "Terminal":
         show("#id_TerminalWrapper");
@@ -64,57 +65,95 @@ function Sidebar() {
         break;
     }
   });
-}
-
-function templeteSidebarComment(id, addr, comment) {
-  let item = "";
-  item += "<div class='comment-content' target='#" + id + "' title='" + addr + "'>";
-  item += "<small class='comment-memory'>" + addr + "</small>";
-  item += "<div class='comment-summary'> # " + comment + "</div>";
-  item += "</div>";
-  return item;
-}
-
-function templateSidebarCommentList(funcName) {
-  let tab_id = $("#id_tabContainer li[title=" + funcName + "]").attr("counter");
-  let $gComments = $("#cfgGrp-" + tab_id + " g.gComment");
-  let item = "<div class='comment-section' value='" + funcName + "'>";
-  item += "<div class='comment-function'>"
-  item += "<i style='width: 10px' class='comment-arrow fas fa-caret-down'></i>";
-  item += funcName;
-  item += "</div>";
-  item += "<div class='comment-list'>"
-  for (let i = 0; i < $gComments.length; i++) {
-    let $gComment = $($gComments[i]);
-    let id = $gComment.attr("id");
-    item += templeteSidebarComment(id, $gComment.parent().find(".stmt").text().split(" ")[0],
-      $gComment.parent().find(".commentText").text());
+  function templeteSidebarComment(id, addr, comment) {
+    let item = "";
+    item += "<div class='comment-content' target='#" + id + "' title='" + addr + "'>";
+    item += "<small class='comment-memory'>" + addr + "</small>";
+    item += "<div class='comment-summary'> # " + comment + "</div>";
+    item += "</div>";
+    return item;
   }
-  item += "</div>";
-  item += "</div>";
 
-  return item;
-}
+  function templateSidebarCommentList(funcName) {
+    let tab_id = $("#id_tabContainer li[title=" + funcName + "]").attr("counter");
+    let $gComments = $("#cfgGrp-" + tab_id + " g.gComment");
+    let item = "<div class='comment-section' value='" + funcName + "'>";
+    item += "<div class='comment-function'>"
+    item += "<i style='width: 10px' class='comment-arrow fas fa-caret-down'></i>";
+    item += funcName;
+    item += "</div>";
+    item += "<div class='comment-list'>"
+    for (let i = 0; i < $gComments.length; i++) {
+      let $gComment = $($gComments[i]);
+      let id = $gComment.attr("id");
+      item += templeteSidebarComment(id, $gComment.parent().find(".stmt").text().split(" ")[0],
+        $gComment.parent().find(".commentText").text());
+    }
+    item += "</div>";
+    item += "</div>";
 
-function setSidebarComments() {
-  $("#id_CommentList").empty();
-  let tablist = $("#id_tabContainer li");
-  let item = "";
-  for (let i = 0; i < tablist.length; i++) {
-    let tab = tablist[i];
-    item += templateSidebarCommentList($(tab).attr("title"));
+    return item;
   }
-  $("#id_CommentList").append(item);
+
+  function setSidebarComments() {
+    $("#id_CommentList").empty();
+    let tablist = $("#id_tabContainer li");
+    let item = "";
+    for (let i = 0; i < tablist.length; i++) {
+      let tab = tablist[i];
+      item += templateSidebarCommentList($(tab).attr("title"));
+    }
+    $("#id_CommentList").append(item);
+  }
+
+  function addComment(funcName, addr, comment) {
+    let item = "<div class='comment-list'>";
+    item += "<div class='comment-content' title='" + addr + "'>";
+    item += "<small class='comment-memory'>" + addr + "</small>";
+    item += "<div class='comment-summary'> # " + comment + "</div>";
+    item += "</div>";
+    item += "</div>";
+    $("#id_CommentList .comment-section[value='" + funcName + "']").append(item);
+  }
+  return {
+    setSidebarComments: setSidebarComments,
+    addComment: addComment
+  }
 }
 
-function addComment(funcName, addr, comment) {
-  let item = "<div class='comment-list'>";
-  item += "<div class='comment-content' title='" + addr + "'>";
-  item += "<small class='comment-memory'>" + addr + "</small>";
-  item += "<div class='comment-summary'> # " + comment + "</div>";
-  item += "</div>";
-  item += "</div>";
-  $("#id_CommentList .comment-section[value='" + funcName + "']").append(item);
+function resizeSidebar(callback) {
+  const minimum_size = 100;
+  const resizer = document.getElementById("id_resize-sidebar");
+  const element = document.getElementById("id_sidebar-content");
+  let original_mouse_x = 0;
+  let original_width = 0;
+  resizer.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+    original_width = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
+    original_mouse_x = e.pageX;
+    window.addEventListener('mousemove', resize);
+    window.addEventListener('mouseup', stopResize);
+  });
+  function resize(e) {
+    const width = original_width + (e.pageX - original_mouse_x);
+    if (width > minimum_size) {
+      element.style.width = width + 'px';
+      element.style.minWidth = width + 'px';
+      element.style.maxWidth = width + 'px';
+      $(".sidebar-content-item").each(function () {
+        $(this).css("max-width", width);
+        $(this).css("min-width", width);
+        $(this).css("width", width);
+        callback();
+      });
+    }
+  }
+  function stopResize() {
+    window.removeEventListener('mousemove', resize);
+  }
+  return {
+    resizeSidebar: resizeSidebar
+  }
 }
 
 Sidebar();
