B2R2-org/B2R2

commit d8786119403b098467cb29cde583dd7cb10dcae4

[EVM] Add codecopy heuristic

src/BinGraph/BinaryApparatus.fs
@@ -172,10 +172,15 @@ module BinaryApparatus =
         addAddrLeader fallAddr i acc
       | _ -> acc) acc
 
-  let private updateMissingInstructions hdl acc (instrMap: InstrMap) =
-    acc.Leaders
-    |> Set.filter (fun l -> not <| instrMap.ContainsKey l.Point.Address)
-    |> InstrMap.update hdl instrMap
+  let rec private findLeaders hdl acc (instrMap: InstrMap) =
+    let acc = instrMap |> Seq.fold foldStmts acc
+    let oldCount = instrMap.Count
+    let instrMap =
+      acc.Leaders
+      |> Set.filter (fun l -> not <| instrMap.ContainsKey l.Point.Address)
+      |> InstrMap.update hdl instrMap
+    if oldCount <> instrMap.Count then findLeaders hdl acc instrMap
+    else struct (instrMap, acc)
 
   let private initBinApp hdl auxEntries =
     let initial = getInitialEntryPoints hdl
@@ -188,13 +193,9 @@ module BinaryApparatus =
 #if DEBUG
     printfn "[*] Loaded basic information."
 #endif
-    (* Then, find all possible leaders by scanning lifted IRs. This is necessary
-       because LowUIR may have intra-instruction branches. *)
-    let acc = instrMap |> Seq.fold foldStmts acc
-#if DEBUG
-    printfn "[*] Update instruction information."
-#endif
-    let instrMap = updateMissingInstructions hdl acc instrMap
+    (* Then, find all possible leaders by scanning lifted IRs. We need to do
+       this at a IR-level because LowUIR may have intra-instruction branches. *)
+    let struct (instrMap, acc) = findLeaders hdl acc instrMap
     let calleeMap = CalleeMap.build hdl acc.FunctionAddrs instrMap
 #if DEBUG
     printfn "[*] The apparatus is ready to use."

src/BinGraph/BinEssence.fs
@@ -45,6 +45,7 @@ type BinEssence = {
 with
   static member private PostAnalysis hdl scfg app =
     [ (LibcAnalysis () :> IPostAnalysis, "LibC analysis")
+      (EVMCodeCopyAnalysis () :> IPostAnalysis, "EVM codecopy analysis")
       (NoReturnAnalysis () :> IPostAnalysis, "NoReturn analysis") ]
     |> List.fold (fun app (analysis, name) ->
       printfn "[*] %s started." name

src/BinGraph/DisasHeuristic.fs
@@ -28,6 +28,8 @@
 namespace B2R2.BinGraph
 
 open B2R2
+open B2R2.BinIR
+open B2R2.BinIR.LowUIR
 open B2R2.FrontEnd
 open B2R2.ConcEval
 open B2R2.BinGraph.EmulationHelper
@@ -108,3 +110,35 @@ type LibcAnalysis () =
   interface IPostAnalysis with
     member __.Run hdl scfg app =
       LibcAnalysisHelper.recoverLibcEntries hdl scfg app
+
+type EVMCodeCopyAnalysis () =
+  let findCodeCopy stmts =
+    stmts
+    |> Array.tryPick (fun stmt ->
+      match stmt with
+      | Store (_, Num dst,
+                  BinOp (BinOpType.APP, _len,
+                         FuncName "code",
+                         BinOp (BinOpType.CONS, _, Num src, _, _, _), _, _)) ->
+        let dstAddr = BitVector.toUInt64 dst
+        let srcAddr = BitVector.toUInt64 src
+        let offset = srcAddr - dstAddr
+        let pp = ProgramPoint (srcAddr, 0)
+        LeaderInfo.Init (pp, ArchOperationMode.NoMode, offset) |> Some
+      | _ -> None)
+
+  let recoverCopiedCode hdl app =
+    app.InstrMap
+    |> Seq.fold (fun app (KeyValue (_, ins)) ->
+      match ins.Stmts |> findCodeCopy with
+      | None -> app
+      | Some leader ->
+        match app.CalleeMap.Find leader.Point.Address with
+        | None -> BinaryApparatus.update hdl app [leader]
+        | Some _ -> app) app
+
+  interface IPostAnalysis with
+    member __.Run hdl _scfg app =
+      match hdl.FileInfo.ISA.Arch with
+      | Architecture.EVM -> recoverCopiedCode hdl app
+      | _ -> app

src/BinGraph/InstrMap.fs
@@ -95,6 +95,7 @@ module InstrMap =
       | [] -> map
       | leaderInfo :: rest ->
         hdl.ParsingContext.ArchOperationMode <- leaderInfo.Mode
+        hdl.ParsingContext.CodeOffset <- leaderInfo.Offset
         match BinHandler.ParseBBlock hdl leaderInfo.Point.Address with
         | Error _ -> buildLoop rest
         | Ok instrs ->

src/Core/BitVector.fs
@@ -245,7 +245,8 @@ type BitVector =
   [<CompiledName("ToUInt64")>]
   static member toUInt64 bv =
     if bv.Length <= 64<rt> then bv.Num
-    else nSizeErr bv.Length
+    elif bv.BigNum > bigint 0xFFFFFFFFFFFFFFFFUL then nSizeErr bv.Length
+    else bv.BigNum |> uint64
 
   [<CompiledName("ToInt64")>]
   static member toInt64 bv = BitVector.toUInt64 bv |> int64

src/FrontEnd.Tests/Parser.Tests.fs
@@ -6533,7 +6533,8 @@ module EVM =
 
   let private test opcode bytes =
     let reader = BinReader.Init (bytes, Endian.Little)
-    let ins = Parser.parse reader WordSize.Bit64 0UL 0
+    let ctxt = new ParsingContext (ArchOperationMode.NoMode)
+    let ins = Parser.parse reader ctxt WordSize.Bit64 0UL 0
     let opcode' = ins.Info.Opcode
     Assert.AreEqual (opcode', opcode)
 

src/FrontEnd/ARM32/ARM32.fs
@@ -38,6 +38,7 @@ type ARM32TranslationContext (isa) =
   override __.Push _ = B2R2.Utils.impossible ()
   override __.Pop () = B2R2.Utils.impossible ()
   override __.Peek _ = B2R2.Utils.impossible ()
+  override __.Clear () = B2R2.Utils.impossible ()
 
 /// Parser for 32-bit ARM instructions. Parser will return a platform-agnostic
 /// instruction type (Instruction).

src/FrontEnd/ARM64/ARM64.fs
@@ -39,6 +39,7 @@ type ARM64TranslationContext (isa) =
   override __.Push _ = B2R2.Utils.impossible ()
   override __.Pop () = B2R2.Utils.impossible ()
   override __.Peek _ = B2R2.Utils.impossible ()
+  override __.Clear () = B2R2.Utils.impossible ()
 
 /// Parser for 64-bit ARM instructions. Parser will return a platform-agnostic
 /// instruction type (Instruction).

commit 005802ea61631d66855978ede69a545cc6f1ca24

[BinGraph] Fix label update bug
The bug was mistakenly introduced in d8786119.

src/BinGraph/BinaryApparatus.fs
@@ -173,6 +173,7 @@ module BinaryApparatus =
       | _ -> acc) acc
 
   let rec private findLeaders hdl acc (instrMap: InstrMap) =
+    let acc = { acc with Labels = instrMap |> Seq.fold findLabels Map.empty }
     let acc = instrMap |> Seq.fold foldStmts acc
     let oldCount = instrMap.Count
     let instrMap =
@@ -187,9 +188,8 @@ module BinaryApparatus =
     let leaders = auxEntries |> Seq.fold (fun set e -> Set.add e set) initial
     let instrMap = InstrMap.build hdl leaders
     (* First, recursively parse all possible instructions. *)
-    let lblmap = instrMap |> Seq.fold findLabels Map.empty
     let funAddrs = leaders |> Seq.map (fun e -> e.Point.Address) |> Set.ofSeq
-    let acc = { Labels = lblmap; Leaders = leaders; FunctionAddrs = funAddrs }
+    let acc = { Labels = Map.empty; Leaders = leaders; FunctionAddrs = funAddrs }
 #if DEBUG
     printfn "[*] Loaded basic information."
 #endif
@@ -201,7 +201,7 @@ module BinaryApparatus =
     printfn "[*] The apparatus is ready to use."
 #endif
     { InstrMap = instrMap
-      LabelMap = lblmap
+      LabelMap = acc.Labels
       LeaderInfos = acc.Leaders
       CallerMap = CallerMap.build calleeMap
       CalleeMap = calleeMap