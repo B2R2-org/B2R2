7de0b69ed1bff1a99aefdb23715175c818c686d0
Malik Ashebani <malikashebani@gmail.com>
2020-01-17 17:32:45 +0200

    [Core] Create LEB128 Codec

---
+++ src/Core.Tests/LEB128CodecTests.fs
@@ -0,0 +1,105 @@
+﻿module B2R2.Core.Tests.LEB128
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+open B2R2.LEB128
+open System
+
+[<TestClass>]
+type TestClass () =
+
+  [<TestMethod>]
+  member __.``decodeUInt64 Test`` () =
+    let u64 = [|
+      ([| 0x00uy; |], 0x00UL)
+      ([| 0x7fuy; |], 0x7fUL)
+      ([| 0x80uy; 0x01uy; |], 0x80UL)
+      ([| 0xffuy; 0x01uy; |], 0xffUL)
+      ([| 0x9duy; 0x12uy; |], 0x091dUL)
+      ([| 0x97uy; 0xdeuy; 0x03uy; |], 0xef17UL)
+      ([| 0xe5uy; 0x8euy; 0x26uy; |], 0x098765UL)
+      ([| 0xffuy; 0xffuy; 0x03uy; |], 0xffffUL)
+      ([| 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0x01uy; |], 18446744073709551615UL)
+      ([| 0x83uy; 0x00uy; |], 0x03UL)
+    |]
+    for arr, res in u64 do
+      let v, c = decodeUInt64 arr
+      Assert.AreEqual(res, v)
+
+  [<TestMethod>]
+  member __.``decodeUInt32 Test`` () =
+    let u32 = [|
+      ([| 0x00uy; |], 0x00u)
+      ([| 0x7fuy; |], 0x7fu)
+      ([| 0x80uy; 0x01uy; |], 0x80u)
+      ([| 0xffuy; 0x01uy; |], 0xffu)
+      ([| 0x9duy; 0x12uy; |], 0x091du)
+      ([| 0x97uy; 0xdeuy; 0x03uy; |], 0xef17u)
+      ([| 0xe5uy; 0x8euy; 0x26uy; |], 0x098765u)
+      ([| 0xffuy; 0xffuy; 0x03uy; |], 0xffffu)
+      ([| 0x83uy; 0x00uy; |], 0x03u)
+    |]
+    for arr, res in u32 do
+      let v, c = decodeUInt32 arr
+      Assert.AreEqual(res, v)
+
+  [<TestMethod>]
+  member __.``decodeSInt64 Test`` () =
+    let s64 = [|
+        ([| 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0xffuy; 0x00uy; |], 9223372036854775807L)
+        ([| 0x97uy; 0xdeuy; 0x03uy; |], 0xef17L)
+        ([| 0xC0uy; 0x00uy; |], 0x40L)
+        ([| 0x3fuy; |], 0x3fL)
+        ([| 0x01uy; |], 1L)
+        ([| 0x00uy; |], 0L)
+        ([| 0x7fuy; |], -1L)
+        ([| 0x40uy; |], -64L)
+        ([| 0xbfuy; 0x7fuy; |], -65L)
+        ([| 0x9Buy; 0xF1uy; 0x59uy; |], -624485L)
+        ([| 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x7fuy; |], -9223372036854775808L)
+        ([| 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x7fuy; |], -268435456L)
+    |]
+    for arr, res in s64 do
+        let v, c = decodeSInt64 arr
+        Assert.AreEqual(res, v)
+
+  [<TestMethod>]
+  member __.``decodeSInt32 Test`` () =
+    let s32 = [|
+        ([| 0x97uy; 0xdeuy; 0x03uy; |], 0xef17)
+        ([| 0xC0uy; 0x00uy; |], 0x40)
+        ([| 0x3fuy; |], 0x3f)
+        ([| 0x01uy; |], 1)
+        ([| 0x00uy; |], 0)
+        ([| 0x7fuy; |], -1)
+        ([| 0x40uy; |], -64)
+        ([| 0xbfuy; 0x7fuy; |], -65)
+        ([| 0x9Buy; 0xF1uy; 0x59uy; |], -624485)
+        ([| 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x7fuy; |], -268435456)
+    |]
+    for arr, res in s32 do
+        let v, c = decodeSInt32 arr
+        Assert.AreEqual(res, v)
+
+  [<TestMethod>]
+  member __.``Overflow handling Test`` () =
+    let overflow = [|
+      [| 0xffuy; |]
+      [| 0x80uy; 0x80uy; |]
+      [| 0xffuy; 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x80uy; 0x7fuy; |]
+    |]
+    let decodeOverflowed func bytes =
+      try
+        func bytes |> ignore
+        false
+      with
+        | :? ArgumentException -> true
+        | _ -> false
+    let u64Result = Array.map (fun arr -> decodeOverflowed decodeUInt64 arr) overflow
+    Assert.IsTrue (Array.forall (fun ov -> ov) u64Result)
+    let u32Result = Array.map (fun arr -> decodeOverflowed decodeUInt32 arr) overflow
+    Assert.IsTrue (Array.forall (fun ov -> ov) u32Result)
+    let s64Result = Array.map (fun arr -> decodeOverflowed decodeSInt64 arr) overflow
+    Assert.IsTrue (Array.forall (fun ov -> ov) s64Result)
+    let s32Result = Array.map (fun arr -> decodeOverflowed decodeSInt32 arr) overflow
+    Assert.IsTrue (Array.forall (fun ov -> ov) s32Result)
---
+++ src/Core/BinCodec.fs
@@ -0,0 +1,48 @@
+﻿namespace B2R2
+
+/// Little Endian Base 128 Codec
+module LEB128 =
+  [<Literal>]
+  let max32LEB128Length = 5
+  [<Literal>]
+  let max64LEB128Length = 10
+
+  let inline private decodeLEB128 (bytes: byte []) (castValue: byte -> 'T) maxLength extendSign (signExtension: 'T) =
+    let rec decodeLoop offset value currentByte length =
+      let newValue = value ||| (castValue (currentByte &&& 0x7fuy) <<< (offset * 7))
+      if currentByte &&& 0x80uy <> 0uy && offset = length - 1 then
+        invalidArg "bytes" "LEB128 Overflow"
+      else
+        if currentByte &&& 0x80uy = 0uy then
+          let finalValue = extendSign currentByte offset newValue signExtension maxLength
+          (finalValue, uint8 (offset + 1))
+        else
+          decodeLoop (offset + 1) newValue bytes.[offset + 1] length
+    if bytes.Length = 0 then
+      invalidArg "bytes" "Invalid buffer length"
+    else
+      let len = if bytes.Length > maxLength then maxLength else bytes.Length
+      decodeLoop 0 (castValue 0uy) bytes.[0] len
+
+  let inline private extendSign currentByte offset (currentValue: 'T) (signExtension: 'T) maxLength =
+    if currentByte &&& 0x40uy <> 0uy then
+      let shiftOffset = if offset < (maxLength - 1) then offset + 1 else offset
+      signExtension <<< (7 * (shiftOffset)) ||| currentValue
+    else
+      currentValue
+
+  [<CompiledName("DecodeUInt64")>]
+  let decodeUInt64 (bytes: byte []) =
+    decodeLEB128 bytes uint64 max64LEB128Length (fun b o v s l -> v) 0UL
+
+  [<CompiledName("DecodeUInt32")>]
+  let decodeUInt32 (bytes: byte []) =
+    decodeLEB128 bytes uint32 max32LEB128Length (fun b o v s l -> v) 0u
+
+  [<CompiledName("DecodeSInt64")>]
+  let decodeSInt64 (bytes: byte []) =
+    decodeLEB128 bytes int64 max64LEB128Length extendSign 0xFFFFFFFFFFFFFFFFL
+
+  [<CompiledName("DecodeSInt32")>]
+  let decodeSInt32 (bytes: byte []) =
+    decodeLEB128 bytes int32 max32LEB128Length extendSign 0xFFFFFFFF
