358c8018f41b46ad966159850b1897d6812ba1de
Malik Ashebani <malikashebani@gmail.com>
2020-01-21 17:50:55 +0200

    [Core] Refactor and Unit-test LEB128 Codec

--- src/Core/BinCodec.fs
+++ src/Core/BinCodec.fs
@@ -7,56 +7,42 @@ module LEB128 =
   [<Literal>]
   let max64LEB128Length = 10
 
-  let inline private decodeUnsignedInt (bytes: byte []) (castValue: byte -> 'T) maxLength =
+  let inline private decodeLEB128 (bytes: byte []) (castValue: byte -> 'T) maxLength extendSign (signExtension: 'T) =
     let rec decodeLoop offset value currentByte length =
       let newValue = value ||| (castValue (currentByte &&& 0x7fuy) <<< (offset * 7))
       if currentByte &&& 0x80uy <> 0uy && offset = length - 1 then
-        Error "LEB128 Overflow"
+        invalidArg "bytes" "LEB128 Overflow"
       else
         if currentByte &&& 0x80uy = 0uy then
-          Ok (newValue, uint8 (offset + 1))
+          let finalValue = extendSign currentByte offset newValue signExtension maxLength
+          (finalValue, uint8 (offset + 1))
         else
           decodeLoop (offset + 1) newValue bytes.[offset + 1] length
     if bytes.Length = 0 then
-      Error "Invalid buffer length"
+      invalidArg "bytes" "Invalid buffer length"
     else
       let len = if bytes.Length > maxLength then maxLength else bytes.Length
       decodeLoop 0 (castValue 0uy) bytes.[0] len
 
-  let inline private decodeSignedInt (bytes: byte []) (castValue: byte -> 'T) maxLength signExtension =
-    let rec decodeLoop offset value bits currentByte length =
-      let newValue = value ||| (castValue (currentByte &&& 0x7fuy) <<< bits)
-      if currentByte &&& 0x80uy <> 0uy && offset = length - 1 then
-        Error "LEB128 Overflow"
-      else
-        if currentByte &&& 0x80uy = 0uy then
-          let finalValue =
-            if currentByte &&& 0x40uy <> 0uy then
-              let shiftOffset = if offset < (maxLength - 1) then offset + 1 else offset
-              signExtension <<< (7 * (shiftOffset)) ||| newValue
-            else
-              newValue
-          Ok (finalValue, uint8 (offset + 1))
-        else
-          decodeLoop (offset + 1) newValue (bits + 7) bytes.[offset + 1] length
-    if bytes.Length = 0 then
-      Error "Invalid buffer length"
+  let inline private extendSign currentByte offset (currentValue: 'T) (signExtension: 'T) maxLength =
+    if currentByte &&& 0x40uy <> 0uy then
+      let shiftOffset = if offset < (maxLength - 1) then offset + 1 else offset
+      signExtension <<< (7 * (shiftOffset)) ||| currentValue
     else
-      let len = if bytes.Length > maxLength then maxLength else bytes.Length
-      decodeLoop 0 (castValue 0uy) 0 bytes.[0] len
+      currentValue
 
   [<CompiledName("DecodeUInt64")>]
   let decodeUInt64 (bytes: byte []) =
-    decodeUnsignedInt bytes uint64 max64LEB128Length
+    decodeLEB128 bytes uint64 max64LEB128Length (fun b o v s l -> v) 0UL
 
   [<CompiledName("DecodeUInt32")>]
   let decodeUInt32 (bytes: byte []) =
-    decodeUnsignedInt bytes uint32 max32LEB128Length
+    decodeLEB128 bytes uint32 max32LEB128Length (fun b o v s l -> v) 0u
 
   [<CompiledName("DecodeSInt64")>]
   let decodeSInt64 (bytes: byte []) =
-    decodeSignedInt bytes int64 max64LEB128Length 0xFFFFFFFFFFFFFFFFL
+    decodeLEB128 bytes int64 max64LEB128Length extendSign 0xFFFFFFFFFFFFFFFFL
 
   [<CompiledName("DecodeSInt32")>]
   let decodeSInt32 (bytes: byte []) =
-    decodeSignedInt bytes int32 max32LEB128Length 0xFFFFFFFF
+    decodeLEB128 bytes int32 max32LEB128Length extendSign 0xFFFFFFFF
