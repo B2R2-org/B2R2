e47ff31242110508f9848ffe40f83ccc1b0ac0d6
Sang Kil Cha <sangkilc@kaist.ac.kr>
2020-04-08 20:53:36 +0900

    [Core] Add a simple Logger class

---
+++ src/Core/Logger.fs
@@ -0,0 +1,63 @@
+(*
+  B2R2 - the Next-Generation Reversing Platform
+
+  Copyright (c) SoftSec Lab. @ KAIST, since 2016
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*)
+
+namespace B2R2
+
+open System.IO
+
+/// How verbose do we want to log messages?
+type LogLevel =
+  /// Most succint = level 1.
+  | L1 = 1
+  /// Normal = level 2.
+  | L2 = 2
+  /// Verbose = level 3.
+  | L3 = 3
+  /// Most verbose = level 4.
+  | L4 = 4
+
+/// Basic logging facility.
+type ILogger =
+  /// Log string (without newline).
+  abstract Log: string * ?lev:LogLevel -> unit
+
+  /// Log string with a new line.
+  abstract LogLine: string * ?lev:LogLevel -> unit
+
+/// Log to a file.
+type FileLogger(filepath, ?level: LogLevel) =
+  let fs = File.CreateText (filepath, AutoFlush = true)
+  let llev = defaultArg level LogLevel.L2
+
+  override __.Finalize () =
+    fs.Close ()
+
+  interface ILogger with
+    member __.Log (str, ?lvl) =
+      let lvl = defaultArg lvl LogLevel.L2
+      if lvl <= llev then fs.Write str else ()
+
+    member __.LogLine (str, ?lvl) =
+      let lvl = defaultArg lvl LogLevel.L2
+      if lvl <= llev then fs.WriteLine str else ()
