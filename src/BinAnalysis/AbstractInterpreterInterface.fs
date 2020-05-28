4097db578b1f695218bc20b9f99e665bd1621fc7
Sang Kil Cha <sangkilc@kaist.ac.kr>
2020-05-28 23:27:55 +0900

    [Misc] Remove unused project

--- src/BinAnalysis/AbstractInterpreterInterface.fs
+++
@@ -1,9 +0,0 @@
-namespace B2R2.BinAnalysis
-
-open System
-open B2R2.BinIR.LowUIR
-
-type IAbstractInterpreter<'astate> =
-  abstract AbstractLFP : Func<'astate, 'astate> -> 'astate -> 'astate -> 'astate
-  abstract InterpStmt : 'astate -> Stmt -> 'astate
-  abstract InterpProg : Stmt [] list -> 'astate
