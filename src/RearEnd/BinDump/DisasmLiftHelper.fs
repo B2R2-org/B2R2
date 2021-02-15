66c83b9fdb819d37d5f31f149bbb1ca9de798df5
Jaeseung Choi <jschoi.2022@gmail.com>
2021-02-15 13:42:45 +0900

    [BinDump] Fix instruction alignment

--- src/RearEnd/BinDump/DisasmLiftHelper.fs
+++ src/RearEnd/BinDump/DisasmLiftHelper.fs
@@ -82,7 +82,12 @@ let getInstructionAlignment hdl (ctxt: ParsingContext) =
     match ctxt.ArchOperationMode with
     | ArchOperationMode.ThumbMode -> 2
     | _ -> 4
-  | _ -> 4
+  | Arch.AARCH64 -> 4
+  | Arch.MIPS1 | Arch.MIPS2 | Arch.MIPS3 | Arch.MIPS4 | Arch.MIPS5
+  | Arch.MIPS32 | Arch.MIPS32R2 | Arch.MIPS32R6
+  | Arch.MIPS64 | Arch.MIPS64R2 | Arch.MIPS64R6 -> 4
+  | Arch.EVM -> 1
+  | _ -> Utils.futureFeature ()
 
 let convertToHexStr bytes =
   bytes
