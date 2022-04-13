e3ef3980fa202c3326b5a98c262a2067924e6adb
Sang Kil Cha <sangkilc@kaist.ac.kr>
2022-04-13 20:49:29 +0900

    [Intel] Fix x87 lifting
    Closes #367

--- src/FrontEnd/BinLifter/Intel/IntelX87Lifter.fs
+++ src/FrontEnd/BinLifter/Intel/IntelX87Lifter.fs
@@ -246,7 +246,7 @@ let fist ins insLen ctxt doPop =
   let oprExpr = transOneOpr ins insLen ctxt
   let oprSize = TypeCheck.typeOf oprExpr
   let struct (st0b, st0a) = getFPUPseudoRegVars ctxt R.ST0
-  let tmp0 = !*ir 64<rt>
+  let tmp0 = !*ir oprSize
   let rcField = !*ir 2<rt> (* Rounding Control *)
   let num2 = numI32 2 2<rt>
   let cst00 = AST.cast CastKind.FtoIRound oprSize tmp0
@@ -254,7 +254,7 @@ let fist ins insLen ctxt doPop =
   let cst10 = AST.cast CastKind.FtoICeil oprSize tmp0
   let cst11 = AST.cast CastKind.FtoITrunc oprSize tmp0
   !<ir insLen
-  !?ir (castFrom80Bit tmp0 64<rt> st0b st0a)
+  !?ir (castFrom80Bit tmp0 oprSize st0b st0a)
   !!ir (rcField := AST.extract (!.ctxt R.FCW) 2<rt> 10)
   !!ir (tmp0 := AST.ite (rcField == AST.num0 2<rt>) cst00 cst11)
   !!ir (tmp0 := AST.ite (rcField == AST.num1 2<rt>) cst01 tmp0)
