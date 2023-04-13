4bcefaf0699d41ec8e51d07032be81143c78ba8f
Hee Dong Yang <heedong@kaist.ac.kr>
2023-04-13 14:10:16 +0900

    [PPC32] Fix mtfsf parsing

--- src/FrontEnd/BinLifter/PPC32/PPC32Parser.fs
+++ src/FrontEnd/BinLifter/PPC32/PPC32Parser.fs
@@ -1859,11 +1859,10 @@ let parseMFFSx bin =
 let parseMTFSFx bin =
   let fm = extract bin 24u 17u |> uint64 |> OprImm
   let frb = getFPRegister (extract bin 15u 11u) |> OprReg
+  (* FIXME: PowerISA v3.1 *)
   match pickBit bin 0u with
-  | 0b0u when concat (pickBit bin 25u) (pickBit bin 16u) 1 = 0u ->
-    struct (Op.MTFSF, TwoOperands (fm, frb))
-  | 0b1u when concat (pickBit bin 25u) (pickBit bin 16u) 1 = 0u ->
-    struct (Op.MTFSFdot, TwoOperands (fm, frb))
+  | 0b0u -> struct (Op.MTFSF, TwoOperands (fm, frb))
+  | 0b1u -> struct (Op.MTFSFdot, TwoOperands (fm, frb))
   | _ -> raise ParsingFailureException
 
 let parse3F bin =
