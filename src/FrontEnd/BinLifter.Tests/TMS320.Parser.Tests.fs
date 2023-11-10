425500106384bff2cf0e07e6c6fb4942ad99fa6d
Su Geun Ji <jsgtwins1@kaist.ac.kr>
2023-11-10 14:24:12 +0900

    [TMS320] Rewrite unit test

--- src/FrontEnd/BinLifter.Tests/TMS320.Parser.Tests.fs
+++ src/FrontEnd/BinLifter.Tests/TMS320.Parser.Tests.fs
@@ -27,8 +27,24 @@ module B2R2.FrontEnd.Tests.TMS320
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open B2R2
 open B2R2.FrontEnd.BinLifter.TMS320C6000
+open type Opcode
+open type Register
 
-let private test opcode unit oprs (bytes: byte[]) =
+/// Shortcut for creating operands.
+type O =
+  static member Reg (r) =
+    OpReg r
+
+  static member Imm (v) =
+    Immediate v
+
+  static member RegPair (r1, r2) =
+    RegisterPair (r1, r2)
+
+  static member Mem (r, modType, offset) =
+    OprMem (r, modType, offset)
+
+let private test unit (bytes: byte[]) (opcode, oprs) =
   let reader = BinReader.Init Endian.Little
   let span = System.ReadOnlySpan bytes
   let mutable inpar = false
@@ -37,70 +53,91 @@ let private test opcode unit oprs (bytes: byte[]) =
   Assert.AreEqual (ins.Info.FunctionalUnit, unit)
   Assert.AreEqual (ins.Info.Operands, oprs)
 
+let private operandsFromArray oprList =
+  let oprs = Array.ofList oprList
+  match oprs.Length with
+  | 0 -> NoOperand
+  | 1 -> OneOperand oprs[0]
+  | 2 -> TwoOperands (oprs[0], oprs[1])
+  | 3 -> ThreeOperands (oprs[0], oprs[1], oprs[2])
+  | 4 -> FourOperands (oprs[0], oprs[1], oprs[2], oprs[3])
+  | _ -> Utils.impossible ()
+
+let private ( ** ) opcode oprList = (opcode, operandsFromArray oprList)
+
+let private ( ++ ) byteString pair = (ByteArray.ofHexString byteString, pair)
+
 /// .D Unit Instructions
 [<TestClass>]
 type DUnitTest () =
   [<TestMethod>]
-  member __.``[TMS320] .D Unit Insturctions Parse Test`` () =
-    test Opcode.ADD D1XUnit
-         (ThreeOperands (OpReg R.A1, OpReg R.B12, OpReg R.A6))
-         [| 0xB0uy; 0x3Auy; 0x30uy; 0x03uy |]
+  member __.``[TMS320] .D Unit Insturctions Parse Test (1)`` () =
+    "B03a3003"
+    ++ ADD ** [ O.Reg A1; O.Reg B12; O.Reg A6 ]
+    ||> test D1XUnit
 
-    test Opcode.LDB D1Unit // *-A5[4],A7
-         (TwoOperands (OprMem (R.A5, NegativeOffset, UCst5 4UL),
-                       OpReg R.A7))
-         [| 0x24uy; 0x80uy; 0x94uy; 0x03uy |]
+  [<TestMethod>]
+  member __.``[TMS320] .D Unit Insturctions Parse Test (2)`` () =
+    "24809403"
+    ++ LDB ** [ O.Mem (A5, NegativeOffset, UCst5 4UL); O.Reg A7 ]
+    ||> test D1Unit
 
 /// .L Unit Instructions
 [<TestClass>]
 type LUnitTest () =
   [<TestMethod>]
-  member __.``[TMS320] .L Unit Insturctions Parse Test`` () =
-    test Opcode.ABS2 L1Unit (TwoOperands (OpReg R.A0, OpReg R.A2))
-         [| 0x58uy; 0x83uy; 0x00uy; 0x01uy |]
+  member __.``[TMS320] .L Unit Insturctions Parse Test (1)`` () =
+    "58830001"
+    ++ ABS2 ** [ O.Reg A0; O.Reg A2 ]
+    ||> test L1Unit
 
-    test Opcode.SUBDP L1XUnit
-         (ThreeOperands (RegisterPair (R.A1, R.A0),
-                         RegisterPair (R.B3, R.B2),
-                         RegisterPair (R.A5, R.A4)))
-         [| 0x38uy; 0x13uy; 0x08uy; 0x02uy |]
+  [<TestMethod>]
+  member __.``[TMS320] .L Unit Insturctions Parse Test (2)`` () =
+    "38130802"
+    ++ SUBDP ** [ O.RegPair (A1, A0); O.RegPair (B3, B2); O.RegPair (A5, A4) ]
+    ||> test L1XUnit
 
 /// .M Unit Instructions
 [<TestClass>]
 type MUnitTest () =
   [<TestMethod>]
-  member __.``[TMS320] .M Unit Insturctions Parse Test`` () =
-    test Opcode.AVG2 M1Unit
-         (ThreeOperands (OpReg R.A0, OpReg R.A1, OpReg R.A2))
-         [| 0xF0uy; 0x04uy; 0x04uy; 0x01uy |]
+  member __.``[TMS320] .M Unit Insturctions Parse Test (1)`` () =
+    "F0040401"
+    ++ AVG2 ** [ O.Reg A0; O.Reg A1; O.Reg A2 ]
+    ||> test M1Unit
 
-    test Opcode.MPY2IR M1XUnit
-         (ThreeOperands (OpReg R.A2, OpReg R.B5,
-                         RegisterPair (R.A9, R.A8)))
-         [| 0xF0uy; 0x53uy; 0x14uy; 0x14uy |]
+  [<TestMethod>]
+  member __.``[TMS320] .M Unit Insturctions Parse Test (2)`` () =
+    "F0531414"
+    ++ MPY2IR ** [ O.Reg A2; O.Reg B5; O.RegPair (A9, A8) ]
+    ||> test M1XUnit
 
 /// .S Unit Instructions
 [<TestClass>]
 type SUnitTest () =
   [<TestMethod>]
-  member __.``[TMS320] .S Unit Insturctions Parse Test`` () =
-    test Opcode.ABSDP S1Unit
-         (TwoOperands (RegisterPair (R.A1, R.A0), RegisterPair (R.A3, R.A2)))
-         [| 0x20uy; 0x0Buy; 0x04uy; 0x01uy |]
+  member __.``[TMS320] .S Unit Insturctions Parse Test (1)`` () =
+    "200B0401"
+    ++ ABSDP ** [ O.RegPair (A1, A0); O.RegPair (A3, A2) ]
+    ||> test S1Unit
 
-    test Opcode.SHRU S1Unit
-         (ThreeOperands (RegisterPair (R.A5, R.A4), Immediate 0x0UL,
-                         RegisterPair (R.A1, R.A0)))
-         [| 0x20uy; 0x09uy; 0x10uy; 0x00uy |]
+  [<TestMethod>]
+  member __.``[TMS320] .S Unit Insturctions Parse Test (2)`` () =
+    "20091000"
+    ++ SHRU ** [ O.RegPair (A5, A4); O.Imm 0x0UL; O.RegPair (A1, A0) ]
+    ||> test S1Unit
 
 /// No Unit Instructions
 [<TestClass>]
 type NoUnitTest () =
   [<TestMethod>]
-  member __.``[TMS320] No Unit Insturctions Parse Test`` () =
-    test Opcode.IDLE NoUnit NoOperand
-         [| 0x00uy; 0xE0uy; 0x01uy; 0x00uy |]
+  member __.``[TMS320] No Unit Insturctions Parse Test (1)`` () =
+    "00E00100"
+    ++ IDLE ** [ ]
+    ||> test NoUnit
 
-    test Opcode.NOP NoUnit (OneOperand (Immediate 5UL))
-         [| 0x00uy; 0x80uy; 0x00uy; 0x00uy |]
-
+  [<TestMethod>]
+  member __.``[TMS320] No Unit Insturctions Parse Test (2)`` () =
+    "00800000"
+    ++ NOP ** [ O.Imm 5UL ]
+    ||> test NoUnit
