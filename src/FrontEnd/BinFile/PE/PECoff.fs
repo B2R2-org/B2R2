B2R2-org/B2R2

commit 9ff4be3aebd165590f42a9a052e7c7b600002dbc

[BinFile] Clean up

src/FrontEnd/BinFile/PE/PECoff.fs
@@ -140,13 +140,12 @@ let buildSymbolMaps arr =
     Map.add symb.Name symb byName
   ) (Map.empty, Map.empty)
 
-let getSymbols (stream: Stream) reader (coff: CoffHeader) =
+let getSymbols bytes reader (coff: CoffHeader) =
   let maxCnt = coff.NumberOfSymbols - 1
   let tblOff = coff.PointerToSymbolTable
   let strOff = tblOff + coff.NumberOfSymbols * 18
   let symbs = Array.zeroCreate coff.NumberOfSymbols
-  let buf = readChunk stream (uint64 tblOff) (coff.NumberOfSymbols * 18)
-  let span = ReadOnlySpan buf
+  let span = ReadOnlySpan (bytes, tblOff, coff.NumberOfSymbols * 18)
   let mutable auxcnt = 0
   let mutable cnt = if tblOff = 0 then maxCnt else 0
   while cnt < maxCnt do

commit c713a8af387d2243dbb8ff2f0a9f4f1d559da1db

[BinFile] Fix parsing bug
Regression introduced by 9ff4be3aebd165590f42a9a052e7c7b600002dbc

src/FrontEnd/BinFile/PE/PECoff.fs
@@ -25,6 +25,7 @@
 module internal B2R2.FrontEnd.BinFile.PE.Coff
 
 open System
+open System.Collections.Generic
 open System.Reflection.PortableExecutable
 open System.Runtime.InteropServices
 open B2R2
@@ -140,12 +141,12 @@ let buildSymbolMaps arr =
     Map.add symb.Name symb byName
   ) (Map.empty, Map.empty)
 
-let getSymbols bytes reader (coff: CoffHeader) =
+let getSymbols (bytes: byte[]) reader (coff: CoffHeader) =
   let maxCnt = coff.NumberOfSymbols - 1
   let tblOff = coff.PointerToSymbolTable
   let strOff = tblOff + coff.NumberOfSymbols * 18
-  let symbs = Array.zeroCreate coff.NumberOfSymbols
-  let span = ReadOnlySpan (bytes, tblOff, coff.NumberOfSymbols * 18)
+  let symbs = List<CoffSymbol> ()
+  let span = ReadOnlySpan bytes
   let mutable auxcnt = 0
   let mutable cnt = if tblOff = 0 then maxCnt else 0
   while cnt < maxCnt do
@@ -159,10 +160,11 @@ let getSymbols bytes reader (coff: CoffHeader) =
       let secnum = reader.ReadInt16 (span, offset + 12) |> int
       let typ = reader.ReadInt16 (span, offset + 14) |> parseSymType
       let storage = span[offset + 16] |> parseStorageClass
-      symbs[cnt] <- getCoffSymbol name v secnum typ storage
+      symbs.Add <| getCoffSymbol name v secnum typ storage
       auxcnt <- span[offset + 17] |> int
       cnt <- cnt + 1
-  Seq.choose toPESymbol symbs
+  symbs
+  |> Seq.choose toPESymbol
   |> fun lst ->
     let arr = Array.ofSeq lst
     let byAddr, byName = buildSymbolMaps arr