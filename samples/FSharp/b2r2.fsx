433701f78872b8304b6130f1574cf8f75a25622f
Sang Kil Cha <sangkilc@kaist.ac.kr>
2025-07-22 15:59:00 +0900

    [Sample] Update

--- samples/FSharp/b2r2.fsx
+++ samples/FSharp/b2r2.fsx
@@ -7,7 +7,7 @@
 open B2R2
 open B2R2.FrontEnd.BinInterface
 
-let isa = ISA.OfString "amd64"
+let isa = ISA "amd64"
 let bytes = [| 0x65uy; 0xffuy; 0x15uy; 0x10uy; 0x00uy; 0x00uy; 0x00uy |]
 let handler = BinHandle.Init (isa, bytes)
 let ins = BinHandle.ParseInstr (handler, 0UL)
