B2R2-org/B2R2

commit 26e9241219ffebde74a28ecf790b4f6dc501f074

[RearEnd] Restructure Utils
- Update the printer structures by introducing `IPrinter`
- Use a static type `Terminal` and share it with all apps

src/RearEnd/FileViewer/Program.fs
@@ -28,34 +28,36 @@ open B2R2
 open B2R2.FrontEnd.BinFile
 open B2R2.FrontEnd
 open B2R2.RearEnd.Utils
-open B2R2.RearEnd.FileViewer.Helper
 
 let dumpBasic (file: IBinFile) =
-  let entry =
-    ColoredSegment(Green, String.entryPointToString file.EntryPoint)
-  out.PrintSectionTitle "Basic Information"
-  out.PrintTwoCols("File format:", FileFormat.toString file.Format)
-  out.PrintTwoCols("Architecture:", file.ISA.ToString())
-  out.PrintTwoCols("Endianness:", Endian.toString file.ISA.Endian)
-  out.PrintTwoCols("Word size:", WordSize.toString file.ISA.WordSize + " bit")
-  out.PrintTwoColsWithColorOnSnd("Entry point:", [ entry ])
-  out.PrintLine()
+  let entry = ColoredString(Green, String.entryPointToString file.EntryPoint)
+  Terminal.Out.PrintSectionTitle "Basic Information"
+  Terminal.Out
+  <== TableConfig.DefaultTwoColumn
+  <== [ "File format:"; FileFormat.toString file.Format ]
+  <== [ "Architecture:"; file.ISA.ToString() ]
+  <== [ "Endianness:"; Endian.toString file.ISA.Endian ]
+  <== [ "Word size:"; WordSize.toString file.ISA.WordSize + " bit" ]
+  <=/ [ OutputNormal "Entry point:"; OutputColored entry ]
+  Terminal.Out.PrintLine()
 
 let dumpSecurity (file: IBinFile) =
-  out.PrintSectionTitle "Security Information"
-  out.PrintTwoCols("Stripped binary:", file.IsStripped.ToString())
-  out.PrintTwoCols("DEP (NX) enabled:", file.IsNXEnabled.ToString())
-  out.PrintTwoCols("Relocatable (PIE):", file.IsRelocatable.ToString())
-  out.PrintLine()
+  Terminal.Out.PrintSectionTitle "Security Information"
+  Terminal.Out
+  <== TableConfig.DefaultTwoColumn
+  <== [ "Stripped binary:"; file.IsStripped.ToString() ]
+  <== [ "DEP (NX) enabled:"; file.IsNXEnabled.ToString() ]
+  <=/ [ "Relocatable (PIE):"; file.IsRelocatable.ToString() ]
+  Terminal.Out.PrintLine()
 
 let dumpSpecific opts (file: IBinFile) title elf pe mach =
-  out.PrintSectionTitle title
+  Terminal.Out.PrintSectionTitle title
   match file with
   | :? ELFBinFile as file -> elf opts file
   | :? PEBinFile as file -> pe opts file
   | :? MachBinFile as file -> mach opts file
   | _ -> Terminator.futureFeature ()
-  out.PrintLine()
+  Terminal.Out.PrintLine()
 
 let dumpFileHeader (opts: FileViewerOpts) (file: IBinFile) =
   dumpSpecific opts file "File Header Information"
@@ -160,8 +162,12 @@ let dumpSharedLibs (opts: FileViewerOpts) (file: IBinFile) =
     ELFViewer.badAccess PEViewer.badAccess MachViewer.dumpSharedLibs
 
 let printFileName filepath =
-  [ Green, "["; Yellow, filepath; Green, "]" ] |> out.PrintLine
-  out.PrintLine()
+  ColoredString()
+    .Add(Green, "[")
+    .Add(Yellow, filepath)
+    .Add(Green, "]")
+  |> Terminal.Out.PrintLine
+  Terminal.Out.PrintLine()
 
 let printBasic file =
   dumpBasic file
@@ -234,14 +240,14 @@ let dump files opts =
   CmdOpts.SanitizeRestArgs files
   match files with
   | [] ->
-    Printer.PrintErrorToConsole "File(s) must be given."
+    Terminal.Out <=? "File(s) must be given."
     CmdOpts.PrintUsage(ToolName, UsageTail, Cmd.spec)
   | files ->
 #if DEBUG
     let sw = System.Diagnostics.Stopwatch.StartNew()
 #endif
     try files |> List.iter (dumpFile opts)
-    finally out.Flush()
+    finally Terminal.Out.Flush()
 #if DEBUG
     sw.Stop()
     eprintfn "Total time: %f sec." sw.Elapsed.TotalSeconds

src/RearEnd/Assembler/Program.fs
@@ -30,20 +30,17 @@ open B2R2.RearEnd.Utils
 open B2R2.BinIR.LowUIR
 open B2R2.Peripheral.Assembly
 
-/// The console printer.
-let internal out = ConsolePrinter() :> Printer
-
 let [<Literal>] private NormalPrompt = "> "
 
 let private printIns (asm: AsmInterface) addr bs =
   let bCode = (BitConverter.ToString(bs)).Replace("-", "")
   let ins = asm.Parser.Parse(bs, addr)
-  out.PrintLine(sprintf "%08x: %-20s     %s" addr bCode (ins.Disasm()))
+  Terminal.Out.PrintLine(sprintf "%08x: %-20s     %s" addr bCode (ins.Disasm()))
   addr + uint64 (Array.length bs)
 
 let inline private printResult fn = function
   | Ok res -> fn res
-  | Error err -> Printer.PrintErrorToConsole err
+  | Error err -> Terminal.Out <=? err
 
 let getAssemblyPrinter (opts: AssemblerOpts) =
   match opts.Mode with
@@ -59,14 +56,14 @@ let getAssemblyPrinter (opts: AssemblerOpts) =
     let asm = AsmInterface(isa, opts.BaseAddress)
     fun str ->
       asm.LiftLowUIR(true, str)
-      |> printResult (Array.iter (Pp.stmtToString >> out.PrintLine))
+      |> printResult (Array.iter (Pp.stmtToString >> Terminal.Out.PrintLine))
 
 let rec private asmFromStdin (console: FsReadLine.Console) printer str =
   match console.ReadLine() with
   | "" -> asmFromStdin console printer str
   | input when isNull input || input = "q" || input = "quit" ->
-    out.PrintLine("Bye!")
-    out.Flush()
+    Terminal.Out.PrintLine("Bye!")
+    Terminal.Out.Flush()
   | input ->
     let input = input.Trim()
     let str =
@@ -82,9 +79,15 @@ let rec private asmFromStdin (console: FsReadLine.Console) printer str =
 let showBasicInfo (opts: AssemblerOpts) =
   match opts.Mode with
   | GeneralMode(isa) ->
-    out.PrintLine [ Blue, isa.ToString(); Green, " General Mode" ]
+    ColoredString()
+      .Add(Blue, isa.ToString())
+      .Add(Green, " General Mode")
+    |> Terminal.Out.PrintLine
   | LowUIRMode(isa) ->
-    out.PrintLine [ Blue, isa.ToString(); Green, " LowUIR Mode" ]
+    ColoredString()
+      .Add(Blue, isa.ToString())
+      .Add(Green, " LowUIR Mode")
+    |> Terminal.Out.PrintLine
 
 let private asmFromFiles files printer =
   files

src/RearEnd/BinDump/Program.fs
@@ -36,18 +36,25 @@ let [<Literal>] private ToolName = "bindump"
 let [<Literal>] private UsageTail = "<binary file(s) | -s hexstring>"
 
 let private printFileName (filepath: string) =
-  out.PrintLine(String.wrapSqrdBracket filepath)
-  out.PrintLine()
+  Terminal.COut <=/ String.wrapSqrdBracket filepath
+  Terminal.COut.PrintLine()
 
 let private getTableConfig (isa: ISA) isLift =
-  if isLift then [ LeftAligned 10 ]
+  if isLift then
+    { Indentation = PrinterConst.Indentation
+      ColumnGap = 0
+      Columns = [ LeftAligned 10 ] }
   else
     let addrWidth = WordSize.toByteWidth isa.WordSize * 2
     let binaryWidth =
       match isa with
       | Intel -> 36
       | _ -> 16
-    [ LeftAligned addrWidth; LeftAligned binaryWidth; LeftAligned 10 ]
+    { Indentation = PrinterConst.Indentation
+      ColumnGap = 0
+      Columns = [ LeftAligned addrWidth
+                  LeftAligned binaryWidth
+                  LeftAligned 10 ] }
 
 let private isARM32 (hdl: BinHandle) =
   match hdl.File.ISA with
@@ -87,13 +94,13 @@ let private dumpRawBinary (hdl: BinHandle) (opts: BinDumpOpts) cfg =
   let ptr = hdl.File.GetBoundedPointer hdl.File.BaseAddress
   let prn = makeCodePrinter hdl cfg opts
   prn.Print ptr
-  out.PrintLine()
+  Terminal.COut.PrintLine()
 
 let printHexdump (opts: BinDumpOpts) (hdl: BinHandle) ptr =
   let bytes = hdl.ReadBytes(ptr = ptr, nBytes = ptr.MaxOffset - ptr.Offset + 1)
   let chunkSz = if opts.ShowWide then 32 else 16
   HexDumper.dump chunkSz hdl.File.ISA.WordSize opts.ShowColor ptr.Addr bytes
-  |> Array.iter out.PrintLine
+  |> Array.iter Terminal.COut.PrintLine
 
 let private hasNoContent (file: IBinFile) secName =
   match file with
@@ -104,11 +111,12 @@ let private hasNoContent (file: IBinFile) secName =
   | _ -> false
 
 let dumpHex (hdl: BinHandle) (opts: BinDumpOpts) ptr secName =
-  out.PrintSectionTitle(String.wrapParen secName)
+  Terminal.COut.PrintSectionTitle(String.wrapParen secName)
   if hasNoContent hdl.File secName then
-    out.PrintTwoCols("", "NOBITS section.")
+    Terminal.COut.SetTableConfig TableConfig.DefaultTwoColumn
+    Terminal.COut.PrintRow([ ""; "NOBITS section." ])
   else printHexdump opts hdl ptr
-  out.PrintLine()
+  Terminal.COut.PrintLine()
 
 let private createBinHandleFromPath (opts: BinDumpOpts) filePath =
   BinHandle(filePath, opts.ISA, opts.BaseAddress)
@@ -124,10 +132,10 @@ let private isRawBinary (hdl: BinHandle) =
 
 let private printCodeOrTable (printer: BinPrinter) ptr =
   printer.Print ptr
-  out.PrintLine()
+  Terminal.COut.PrintLine()
 
 let private dumpOneSection (prn: BinPrinter) name ptr =
-  out.PrintSectionTitle(String.wrapParen name)
+  Terminal.COut.PrintSectionTitle(String.wrapParen name)
   printCodeOrTable prn ptr
 
 let private dumpELFSection hdl opts elf tableprn codeprn sec =
@@ -204,17 +212,17 @@ let dumpFile (opts: BinDumpOpts) filepath =
 let dumpFileMode files (opts: BinDumpOpts) =
   match List.partition IO.File.Exists files with
   | [], [] ->
-    Printer.PrintErrorToConsole "File(s) must be given."
+    Terminal.Out <=? "File(s) must be given."
     CmdOpts.PrintUsage(ToolName, UsageTail, Cmd.spec)
   | files, [] -> files |> List.iter (dumpFile opts)
   | _, errs ->
-    Printer.PrintErrorToConsole("File(s) " + errs.ToString() + " not found!")
+    Terminal.Out <=? "File(s) " + errs.ToString() + " not found!"
 
 let private assertBinaryLength isa isThumb hexstr =
   let multiplier = getInstructionAlignment isa isThumb
   if (Array.length hexstr) % multiplier = 0 then ()
   else
-    Printer.PrintErrorToConsole <|
+    Terminal.Out <=?
       "The hex string length must be multiple of " + multiplier.ToString()
     exit 1
 
@@ -230,7 +238,7 @@ let dumpHexStringMode (opts: BinDumpOpts) =
   let len = opts.InputHexStr.Length
   let ptr = BinFilePointer(baseAddr, baseAddr + uint64 len - 1UL, 0, len - 1)
   printer.Print ptr
-  out.PrintLine()
+  Terminal.COut.PrintLine()
 
 let private dump files (opts: BinDumpOpts) =
 #if DEBUG
@@ -241,7 +249,7 @@ let private dump files (opts: BinDumpOpts) =
     if Array.isEmpty opts.InputHexStr then dumpFileMode files opts
     else dumpHexStringMode opts
   finally
-    out.Flush()
+    Terminal.COut.Flush()
 #if DEBUG
   sw.Stop()
   eprintfn "Total dump time: %f sec." sw.Elapsed.TotalSeconds

src/RearEnd/BinExplorer/Program.fs
@@ -34,7 +34,6 @@ open B2R2.MiddleEnd.ControlFlowGraph
 open B2R2.MiddleEnd.ControlFlowAnalysis
 open B2R2.RearEnd.Utils
 open B2R2.RearEnd.Visualization
-open B2R2.RearEnd.BinExplorer.CmdUtils
 
 type BinExplorerOpts(isa) =
   inherit CmdOpts()
@@ -280,7 +279,7 @@ let runCommand cmdMap opts file cmd args =
   let strategies = [| funcId :> ICFGBuildingStrategy<_, _>; cfgRecovery |]
   let brew = BinaryBrew(hdl, exnInfo, strategies)
   Cmd.handle cmdMap brew cmd args
-  |> Array.iter out.Print
+  |> Array.iter Terminal.Out.Print
 
 let [<Literal>] private ToolName = "binexplore"
 

src/RearEnd/Launcher/Program.fs
@@ -29,7 +29,8 @@ open B2R2
 open B2R2.RearEnd.Utils
 
 let showUsage () =
-  Printer.PrintToConsole $"""
+  Terminal.Out
+  <=/ $"""
          ''''''''''
       .;'          ';.
      ;' o      o     ';           888888b.           8888888b.
@@ -77,8 +78,7 @@ Usage: b2r2 [app name]{"\n"}
 let printMyVersion () =
   let asm = Assembly.GetEntryAssembly()
   let attr = asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-  attr.InformationalVersion.ToString()
-  |> Printer.PrintToConsole
+  Terminal.Out <=/ attr.InformationalVersion.ToString()
 
 let handleCommands (cmd: string) (rest: string []) =
   match cmd.ToLowerInvariant() with

src/RearEnd/Transformer/Program.fs
@@ -48,12 +48,12 @@ type private HelpAction(map: Map<string, IAction>) =
     member _.Signature with get() = "'a -> 'b"
     member _.Description with get() = ""
     member _.Transform(_args, _) =
-      Printer.PrintToConsoleLine()
+      Terminal.Out.PrintLine()
       CmdOpts.WriteIntro()
-      Printer.PrintToConsoleLine usage
+      Terminal.Out.PrintLine usage
       map |> Map.iter (fun id act ->
-        Printer.PrintToConsoleLine $"- {id}: {act.Signature}"
-        Printer.PrintToConsoleLine $"{act.Description}")
+        Terminal.Out.PrintLine $"- {id}: {act.Signature}"
+        Terminal.Out.PrintLine $"{act.Description}")
       exit 0
 
 let private accumulateActions map actions =
@@ -119,7 +119,7 @@ let private checkValidityOfCommandGroup cmdgrp =
   if actionIDs |> List.forall (fun actionID -> actionID = fstActionID) then
     ()
   else
-    Printer.PrintErrorToConsole "different actions in the same group."
+    Terminal.Out <=? "different actions in the same group."
     exit 1
 
 let private runCommand actionMap input (cmd: string list) =
@@ -129,26 +129,26 @@ let private runCommand actionMap input (cmd: string list) =
     match Map.tryFind (actionID.ToLowerInvariant()) actionMap with
     | Some act -> act
     | None ->
-      Printer.PrintErrorToConsole $"({actionID}) is not a valid action."
+      Terminal.Out <=? $"({actionID}) is not a valid action."
       exit 1
 #if DEBUG
-  if actionID <> "help" then Printer.PrintToConsoleLine $"[*] {actionID}"
+  if actionID <> "help" then Terminal.Out.PrintLine $"[*] {actionID}"
   else ()
 #endif
   try
     action.Transform(args, input)
   with
     | :? InvalidCastException ->
-      Printer.PrintErrorToConsole $"({actionID}) action type mismatch."
+      Terminal.Out <=? $"({actionID}) action type mismatch."
       exit 1
     | :? NullReferenceException ->
-      Printer.PrintErrorToConsole $"({actionID}) action should follow another."
+      Terminal.Out <=? $"({actionID}) action should follow another."
       exit 1
     | :? ArgumentException as e ->
-      Printer.PrintErrorToConsole $"{e.Message}"
+      Terminal.Out <=? $"{e.Message}"
       exit 1
     | e ->
-      Printer.PrintErrorToConsole $"({actionID}): {e}"
+      Terminal.Out <=? $"({actionID}): {e}"
       exit 1
 
 let inline private unwrap (c: ObjCollection) = c.Values

src/RearEnd/BinDump/DisasmLiftHelper.fs
@@ -32,12 +32,6 @@ open B2R2.FrontEnd.BinFile
 open B2R2.FrontEnd.BinLifter
 open B2R2.RearEnd.Utils
 
-/// The monotonic console printer.
-let internal out = ConsoleCachedPrinter() :> Printer
-
-/// The colorful console printer.
-let internal colorout = ConsolePrinter() :> Printer
-
 let [<Literal>] IllegalStr = "(illegal)"
 
 let getOptimizer (opts: BinDumpOpts) =
@@ -107,13 +101,15 @@ let convertToHexStr bytes =
 
 let printLowUIR (lowUIRStr: string) bytes cfg =
   let hexStr = convertToHexStr bytes |> String.wrapSqrdBracket
-  out.PrintRow(false, cfg, [ hexStr ])
-  out.PrintRow(false, cfg, [ lowUIRStr ])
+  Terminal.COut.SetTableConfig(cfg = cfg)
+  Terminal.COut.PrintRow([ hexStr ])
+  Terminal.COut.PrintRow([ lowUIRStr ])
 
 let printRegularDisasm disasmStr wordSize addr bytes cfg =
   let hexStr = convertToHexStr bytes
   let addrStr = Addr.toString wordSize addr + ":"
-  out.PrintRow(false, cfg, [ addrStr; hexStr; disasmStr ])
+  Terminal.COut.SetTableConfig(cfg = cfg)
+  Terminal.COut.PrintRow([ addrStr; hexStr; disasmStr ])
 
 let regularDisPrinter hdl liftingUnit wordSize showSymbs ptr ins cfg =
   (liftingUnit: LiftingUnit).ConfigureDisassembly(false, showSymbs)
@@ -128,23 +124,25 @@ let regularIRPrinter hdl (liftingUnit: LiftingUnit) optimizer ptr ins cfg =
   printLowUIR lowUIRStr bytes cfg
 
 let convertToDisasmStr (words: AsmWord []) =
-  words
-  |> Array.choose (fun word ->
+  let cs = ColoredString()
+  for word in words do
     match word.AsmWordKind with
-    | AsmWordKind.Address -> None
-    | AsmWordKind.Mnemonic -> Some [ Green, word.AsmWordValue ]
-    | AsmWordKind.Variable -> Some [ Blue, word.AsmWordValue ]
-    | AsmWordKind.Value -> Some [ Red, word.AsmWordValue ]
-    | _ -> Some [ NoColor, word.AsmWordValue ])
-  |> List.concat
+    | AsmWordKind.Address -> ()
+    | AsmWordKind.Mnemonic -> cs.Add(Green, word.AsmWordValue) |> ignore
+    | AsmWordKind.Variable -> cs.Add(Blue, word.AsmWordValue) |> ignore
+    | AsmWordKind.Value -> cs.Add(Red, word.AsmWordValue) |> ignore
+    | _ -> cs.Add(NoColor, word.AsmWordValue) |> ignore
+  cs
 
 let printColorDisasm words wordSize addr bytes cfg =
-  out.Flush()
+  Terminal.COut.Flush()
   let hexStr = convertToHexStr bytes
   let addrStr = Addr.toString wordSize addr + ":"
   let disasStr = convertToDisasmStr words
-  colorout.PrintRow(false, cfg,
-    [ [ Green, addrStr ]; [ NoColor, hexStr ]; disasStr ])
+  Terminal.Out.SetTableConfig(cfg = cfg)
+  Terminal.Out.PrintRow([ ColoredString(Green, addrStr)
+                          ColoredString(NoColor, hexStr)
+                          disasStr ])
 
 let colorDisPrinter (hdl: BinHandle) liftingUnit wordSize _ ptr ins cfg =
   (liftingUnit: LiftingUnit).ConfigureDisassembly false
@@ -163,8 +161,8 @@ let handleInvalidIns hdl (modeSwitch: ARM32.IModeSwitchable) ptr isLift cfg =
 let printFuncSymbol (dict: Dictionary<Addr, string>) addr =
   match (dict: Dictionary<Addr, string>).TryGetValue(addr) with
   | true, name ->
-    out.PrintLineIfPrevLineWasNotEmpty()
-    out.PrintLine(String.wrapAngleBracket name)
+    Terminal.COut.PrintLine()
+    Terminal.COut.PrintLine(String.wrapAngleBracket name)
   | false, _ -> ()
 
 let updateMode dict (modeSwitch: ARM32.IModeSwitchable) addr =

commit d3cf2b2f7f8b7778f2454efd8048136a885aee58

[FileViewer] Use default indentation = 2
Make the output consistent. This commit fixes a regression :bug:
introduced by 26e9241219ffebde74a28ecf790b4f6dc501f074

src/RearEnd/FileViewer/ELFViewer.fs
@@ -77,20 +77,20 @@ let dumpSectionHeaders (opts: FileViewerOpts) (elf: ELFBinFile) =
   let file = elf :> IBinFile
   if opts.Verbose then
     let cfg =
-      { Indentation = 2
-        ColumnGap = 0
-        Columns = [ LeftAligned 4
-                    addrColumn
-                    addrColumn
-                    LeftAligned 24
-                    LeftAligned 14
-                    LeftAligned 12
-                    LeftAligned 8
-                    LeftAligned 10
-                    LeftAligned 4
-                    LeftAligned 4
-                    LeftAligned 6
-                    LeftAligned 20 ] }
+      { TableConfig.DefaultTwoColumn with
+          Indentation = 2
+          Columns = [ LeftAligned 4
+                      addrColumn
+                      addrColumn
+                      LeftAligned 24
+                      LeftAligned 14
+                      LeftAligned 12
+                      LeftAligned 8
+                      LeftAligned 10
+                      LeftAligned 4
+                      LeftAligned 4
+                      LeftAligned 6
+                      LeftAligned 20 ] }
     Terminal.Out
     <== cfg
     <== [ "Num"
@@ -124,9 +124,9 @@ let dumpSectionHeaders (opts: FileViewerOpts) (elf: ELFBinFile) =
     )
   else
     let cfg =
-      { Indentation = 2
-        ColumnGap = 0
-        Columns = [ LeftAligned 4; addrColumn; addrColumn; LeftAligned 24 ] }
+      { TableConfig.DefaultTwoColumn with
+          Indentation = 2
+          Columns = [ LeftAligned 4; addrColumn; addrColumn; LeftAligned 24 ] }
     Terminal.Out
     <== cfg
     <== [ "Num"; "Start"; "End"; "Name" ]
@@ -194,17 +194,17 @@ let printSymbolInfo isVerbose (elf: ELFBinFile) =
   let addrColumn = columnWidthOfAddr elf |> LeftAligned
   if isVerbose then
     let cfg =
-      { Indentation = 2
-        ColumnGap = 0
-        Columns = [ LeftAligned 4
-                    addrColumn
-                    LeftAligned 55
-                    LeftAligned 15
-                    LeftAligned 8
-                    LeftAligned 12
-                    LeftAligned 8
-                    LeftAligned 10
-                    LeftAligned 8 ] }
+      { TableConfig.DefaultTwoColumn with
+          Indentation = 2
+          Columns = [ LeftAligned 4
+                      addrColumn
+                      LeftAligned 55
+                      LeftAligned 15
+                      LeftAligned 8
+                      LeftAligned 12
+                      LeftAligned 8
+                      LeftAligned 10
+                      LeftAligned 8 ] }
     Terminal.Out
     <== cfg
     <== [ "S/D"
@@ -224,13 +224,13 @@ let printSymbolInfo isVerbose (elf: ELFBinFile) =
     |> Array.iter (fun s -> printSymbolInfoVerbose elf s "(d)" cfg)
   else
     let cfg =
-      { Indentation = 2
-        ColumnGap = 0
-        Columns = [ LeftAligned 3
-                    LeftAligned 10
-                    addrColumn
-                    LeftAligned 75
-                    LeftAligned 15 ] }
+      { TableConfig.DefaultTwoColumn with
+          Indentation = 2
+          Columns = [ LeftAligned 3
+                      LeftAligned 10
+                      addrColumn
+                      LeftAligned 75
+                      LeftAligned 15 ] }
     Terminal.Out
     <== cfg
     <== [ "S/D"; "Kind"; "Address"; "Name"; "Lib Name" ]
@@ -268,13 +268,13 @@ let dumpRelocs (_opts: FileViewerOpts) (elf: ELFBinFile) =
 let dumpFunctions (_: FileViewerOpts) (elf: ELFBinFile) =
   let addrColumn = columnWidthOfAddr elf |> LeftAligned
   let cfg =
-    { Indentation = 2
-      ColumnGap = 0
-      Columns = [ LeftAligned 3
-                  LeftAligned 10
-                  addrColumn
-                  LeftAligned 75
-                  LeftAligned 15 ] }
+    { TableConfig.DefaultTwoColumn with
+        Indentation = 2
+        Columns = [ LeftAligned 3
+                    LeftAligned 10
+                    addrColumn
+                    LeftAligned 75
+                    LeftAligned 15 ] }
   for addr in (elf :> IBinFile).GetFunctionAddresses() do
     match elf.Symbols.TryFindSymbol addr with
     | Ok symb -> printSymbolInfoNonVerbose elf symb "" cfg

src/RearEnd/FileViewer/Program.fs
@@ -33,7 +33,6 @@ let dumpBasic (file: IBinFile) =
   let entry = ColoredString(Green, String.ofEntryPointOpt file.EntryPoint)
   Terminal.Out.PrintSectionTitle "Basic Information"
   Terminal.Out
-  <== TableConfig.DefaultTwoColumn
   <== [ "File format:"; FileFormat.toString file.Format ]
   <== [ "Architecture:"; file.ISA.ToString() ]
   <== [ "Endianness:"; Endian.toString file.ISA.Endian ]
@@ -44,7 +43,6 @@ let dumpBasic (file: IBinFile) =
 let dumpSecurity (file: IBinFile) =
   Terminal.Out.PrintSectionTitle "Security Information"
   Terminal.Out
-  <== TableConfig.DefaultTwoColumn
   <== [ "Stripped binary:"; file.IsStripped.ToString() ]
   <== [ "DEP (NX) enabled:"; file.IsNXEnabled.ToString() ]
   <=/ [ "Relocatable (PIE):"; file.IsRelocatable.ToString() ]
@@ -256,4 +254,5 @@ let dump files opts =
 [<EntryPoint>]
 let main args =
   let opts = FileViewerOpts.Default
+  Terminal.Out <=/ { TableConfig.DefaultTwoColumn with Indentation = 2 }
   CmdOpts.parseAndRun dump ToolName UsageTail FileViewerOpts.Spec opts args