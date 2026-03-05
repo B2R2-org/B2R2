66987112bf6484d0be6b3633b7af63930a0f707f
Sang Kil Cha <sangkilc@kaist.ac.kr>
2026-03-05 09:11:48 +0900

    [BinExplore] Add GUI boilerplate

--- src/RearEnd/BinExplore/BinExploreOpts.fs
+++ src/RearEnd/BinExplore/BinExploreOpts.fs
@@ -42,6 +42,8 @@ type BinExploreOpts =
     Port: int
     /// Logging output file.
     LogFile: string option
+    /// Whether to use GUI or not.
+    UseGUI: bool
     /// ISA of the target binary. This is only meaningful for universal (FAT)
     /// binaries because BinHandle will automatically detect file format by
     /// default. When a FAT binary is given, we need to choose which ISA to use
@@ -57,6 +59,7 @@ with
     { IP = "localhost"
       Port = 8282
       LogFile = None
+      UseGUI = true
       ISA = isa
       Verbose = false }
 
@@ -80,6 +83,9 @@ with
       Opt(descr = noCol "Specify <ISA> (e.g., x86) for fat binaries",
           short = "-a", long = "--isa", extra = 1,
           callback = fun opts arg -> { opts with ISA = ISA arg[0] })
+      Opt(descr = noCol "Disable GUI (enabled by default)",
+          long = "--no-gui",
+          callback = fun opts _-> { opts with UseGUI = false })
       Opt(descr = noCol "",
           dummy = true)
       Opt(descr = ColoredString().Add(NoColor, "[")
---
+++ src/RearEnd/BinExplore/GUI.fs
@@ -0,0 +1,72 @@
+(*
+  B2R2 - the Next-Generation Reversing Platform
+
+  Copyright (c) SoftSec Lab. @ KAIST, since 2016
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*)
+
+module internal B2R2.RearEnd.BinExplore.GUI
+
+open Avalonia
+open Avalonia.Themes.Fluent
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.FuncUI
+open Avalonia.FuncUI.Hosts
+open Avalonia.FuncUI.DSL
+open Avalonia.Controls
+
+let update _msg _model = ()
+
+let view (model: unit) (dispatch: unit -> unit) =
+  DockPanel.create [
+    DockPanel.children [
+      TextBlock.create [
+        TextBlock.text "Hello, BinExplore!"
+      ]
+    ]
+  ]
+
+type MainWindow() as this =
+  inherit HostWindow()
+  do
+    base.Title <- "BinExplore"
+    Elmish.Program.mkSimple (fun () -> ()) update view
+    |> Elmish.Program.withHost this
+    |> Elmish.Program.run
+
+type App(arbiter) =
+  inherit Application()
+
+  override this.Initialize() =
+    this.Styles.Add(FluentTheme())
+    this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
+
+  override this.OnFrameworkInitializationCompleted() =
+    match this.ApplicationLifetime with
+    | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+      desktopLifetime.MainWindow <- MainWindow()
+    | _ -> ()
+
+let start arbiter =
+  AppBuilder
+    .Configure<App>(fun () -> App(arbiter))
+    .UsePlatformDetect()
+    .UseSkia()
+    .StartWithClassicDesktopLifetime([||])
