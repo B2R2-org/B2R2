934a0111698a0b51572106226f948bb1e6cf7c3c
Seung Il Jung <sijung@kaist.ac.kr>
2026-04-08 16:16:40 +0900

    [Intel] Add TupleType and update Op/En

--- src/FrontEnd/Intel/ParsingHelper.fs
+++ src/FrontEnd/Intel/ParsingHelper.fs
@@ -194,27 +194,6 @@ and internal SzCond =
   /// Normal conditions. This includes all other size conditions in Table A-1.
   | Normal = 2
 
-/// The tupletype will be referenced in the instruction operand encoding table
-/// in the reference page of each instruction, providing the cross reference for
-/// the scaling factor N to encoding memory addressing operand.
-and internal TupleType =
-  /// Compressed Displacement (DISP8*N) Affected by Embedded Broadcast.
-  | Full = 0
-  | Half = 1
-  /// EVEX DISP8*N for Instructions Not Affected by Embedded Broadcast.
-  | FullMem = 2
-  | Tuple1Scalar = 3
-  | Tuple1Fixed = 4
-  | Tuple2 = 5
-  | Tuple4 = 6
-  | Tuple8 = 7
-  | HalfMem = 8
-  | QuarterMem = 9
-  | EighthMem = 10
-  | Mem128 = 11
-  | MOVDDUP = 12
-  | NA = 13 (* N/A *)
-
 and internal SizeKind =
   | Byte = 0
   | Word = 1
