d211108554247a4f02ce67ca2df78b079674f4d1
Jung Hyun Kim <jidoc01@gmail.com>
2026-06-19 17:06:26 +0900

    [Python] Support EXTENDED_ARG

--- src/FrontEnd/Python/Parsing/Parsing312.fs
+++ src/FrontEnd/Python/Parsing/Parsing312.fs
@@ -31,8 +31,8 @@ open B2R2.FrontEnd.BinFile
 open B2R2.FrontEnd.BinLifter
 open B2R2.FrontEnd.Python
 
-(* LOAD_GLOBAL and LOAD_ATTR encode a flag in the low bit of arg;
-   the actual name index is arg >> 1. *)
+(* LOAD_GLOBAL, LOAD_ATTR, and LOAD_SUPER_ATTR encode a flag in the low bit
+   of arg; the actual name index is arg >> 1. *)
 let private getIndex opcode (rawArg: int) =
   match opcode with
   | Op.LOAD_GLOBAL
@@ -74,9 +74,9 @@ let private getTable (binFile: PythonBinFile) = function
   | _ -> [||]
 
 let private parseOperand opcode (span: ReadOnlySpan<byte>) (reader: IBinReader)
-  (binFile: PythonBinFile) addr instrLen =
+                         binFile addr instrLen extArg =
   let tbl = getTable binFile opcode
-  let rawArg = reader.ReadUInt8(span, 1) |> int
+  let rawArg = (reader.ReadUInt8(span, 1) |> int) ||| extArg
   let idx = getIndex opcode rawArg
   let cons =
     tbl
@@ -87,9 +87,8 @@ let private parseOperand opcode (span: ReadOnlySpan<byte>) (reader: IBinReader)
     | None -> OneOperand(idx, None)
   struct (opcode, opr, instrLen)
 
-let private parseInstruction (span: ReadOnlySpan<byte>) (reader: IBinReader)
-  (bFile: PythonBinFile) addr =
-  let bin = reader.ReadUInt8(span, 0)
+let private parseInstruction (span: ReadOnlySpan<byte>) reader bf addr extArg =
+  let bin = (reader: IBinReader).ReadUInt8(span, 0)
   (* Opcode of Python 3.12 *)
   match bin with
   | 0x0uy -> struct (Op.CACHE, NoOperand, 2u)
@@ -131,124 +130,135 @@ let private parseInstruction (span: ReadOnlySpan<byte>) (reader: IBinReader)
   | 0x55uy -> struct (Op.SETUP_ANNOTATIONS, NoOperand, 2u)
   | 0x57uy -> struct (Op.LOAD_LOCALS, NoOperand, 2u)
   | 0x59uy -> struct (Op.POP_EXCEPT, NoOperand, 2u)
-  | 0x5Auy -> parseOperand Op.STORE_NAME span reader bFile addr 2u
-  | 0x5Buy -> parseOperand Op.DELETE_NAME span reader bFile addr 2u
-  | 0x5Cuy -> parseOperand Op.UNPACK_SEQUENCE span reader bFile addr 2u
-  | 0x5Duy -> parseOperand Op.FOR_ITER span reader bFile addr 2u
-  | 0x5Euy -> parseOperand Op.UNPACK_EX span reader bFile addr 2u
-  | 0x5Fuy -> parseOperand Op.STORE_ATTR span reader bFile addr 10u
-  | 0x60uy -> parseOperand Op.DELETE_ATTR span reader bFile addr 2u
-  | 0x61uy -> parseOperand Op.STORE_GLOBAL span reader bFile addr 2u
-  | 0x62uy -> parseOperand Op.DELETE_GLOBAL span reader bFile addr 2u
-  | 0x63uy -> parseOperand Op.SWAP span reader bFile addr 2u
-  | 0x64uy -> parseOperand Op.LOAD_CONST span reader bFile addr 2u
-  | 0x65uy -> parseOperand Op.LOAD_NAME span reader bFile addr 2u
-  | 0x66uy -> parseOperand Op.BUILD_TUPLE span reader bFile addr 2u
-  | 0x67uy -> parseOperand Op.BUILD_LIST span reader bFile addr 2u
-  | 0x68uy -> parseOperand Op.BUILD_SET span reader bFile addr 2u
-  | 0x69uy -> parseOperand Op.BUILD_MAP span reader bFile addr 2u
-  | 0x6Auy -> parseOperand Op.LOAD_ATTR span reader bFile addr 20u
-  | 0x6Buy -> parseOperand Op.COMPARE_OP span reader bFile addr 2u
-  | 0x6Cuy -> parseOperand Op.IMPORT_NAME span reader bFile addr 2u
-  | 0x6Duy -> parseOperand Op.IMPORT_FROM span reader bFile addr 2u
-  | 0x6Euy -> parseOperand Op.JUMP_FORWARD span reader bFile addr 2u
-  | 0x72uy -> parseOperand Op.POP_JUMP_IF_FALSE span reader bFile addr 2u
-  | 0x73uy -> parseOperand Op.POP_JUMP_IF_TRUE span reader bFile addr 2u
-  | 0x74uy -> parseOperand Op.LOAD_GLOBAL span reader bFile addr 10u
-  | 0x75uy -> parseOperand Op.IS_OP span reader bFile addr 2u
-  | 0x76uy -> parseOperand Op.CONTAINS_OP span reader bFile addr 2u
-  | 0x77uy -> parseOperand Op.RERAISE span reader bFile addr 2u
-  | 0x78uy -> parseOperand Op.COPY span reader bFile addr 2u
-  | 0x79uy -> parseOperand Op.RETURN_CONST span reader bFile addr 2u
-  | 0x7Auy -> parseOperand Op.BINARY_OP span reader bFile addr 4u
-  | 0x7Buy -> parseOperand Op.SEND span reader bFile addr 2u
-  | 0x7Cuy -> parseOperand Op.LOAD_FAST span reader bFile addr 2u
-  | 0x7Duy -> parseOperand Op.STORE_FAST span reader bFile addr 2u
-  | 0x7Euy -> parseOperand Op.DELETE_FAST span reader bFile addr 2u
-  | 0x7Fuy -> parseOperand Op.LOAD_FAST_CHECK span reader bFile addr 2u
-  | 0x80uy -> parseOperand Op.POP_JUMP_IF_NOT_NONE span reader bFile addr 2u
-  | 0x81uy -> parseOperand Op.POP_JUMP_IF_NONE span reader bFile addr 2u
-  | 0x82uy -> parseOperand Op.RAISE_VARARGS span reader bFile addr 2u
-  | 0x83uy -> parseOperand Op.GET_AWAITABLE span reader bFile addr 2u
-  | 0x84uy -> parseOperand Op.MAKE_FUNCTION span reader bFile addr 2u
-  | 0x85uy -> parseOperand Op.BUILD_SLICE span reader bFile addr 2u
+  | 0x5Auy -> parseOperand Op.STORE_NAME span reader bf addr 2u extArg
+  | 0x5Buy -> parseOperand Op.DELETE_NAME span reader bf addr 2u extArg
+  | 0x5Cuy -> parseOperand Op.UNPACK_SEQUENCE span reader bf addr 2u extArg
+  | 0x5Duy -> parseOperand Op.FOR_ITER span reader bf addr 2u extArg
+  | 0x5Euy -> parseOperand Op.UNPACK_EX span reader bf addr 2u extArg
+  | 0x5Fuy -> parseOperand Op.STORE_ATTR span reader bf addr 10u extArg
+  | 0x60uy -> parseOperand Op.DELETE_ATTR span reader bf addr 2u extArg
+  | 0x61uy -> parseOperand Op.STORE_GLOBAL span reader bf addr 2u extArg
+  | 0x62uy -> parseOperand Op.DELETE_GLOBAL span reader bf addr 2u extArg
+  | 0x63uy -> parseOperand Op.SWAP span reader bf addr 2u extArg
+  | 0x64uy -> parseOperand Op.LOAD_CONST span reader bf addr 2u extArg
+  | 0x65uy -> parseOperand Op.LOAD_NAME span reader bf addr 2u extArg
+  | 0x66uy -> parseOperand Op.BUILD_TUPLE span reader bf addr 2u extArg
+  | 0x67uy -> parseOperand Op.BUILD_LIST span reader bf addr 2u extArg
+  | 0x68uy -> parseOperand Op.BUILD_SET span reader bf addr 2u extArg
+  | 0x69uy -> parseOperand Op.BUILD_MAP span reader bf addr 2u extArg
+  | 0x6Auy -> parseOperand Op.LOAD_ATTR span reader bf addr 20u extArg
+  | 0x6Buy -> parseOperand Op.COMPARE_OP span reader bf addr 2u extArg
+  | 0x6Cuy -> parseOperand Op.IMPORT_NAME span reader bf addr 2u extArg
+  | 0x6Duy -> parseOperand Op.IMPORT_FROM span reader bf addr 2u extArg
+  | 0x6Euy -> parseOperand Op.JUMP_FORWARD span reader bf addr 2u extArg
+  | 0x72uy -> parseOperand Op.POP_JUMP_IF_FALSE span reader bf addr 2u extArg
+  | 0x73uy -> parseOperand Op.POP_JUMP_IF_TRUE span reader bf addr 2u extArg
+  | 0x74uy -> parseOperand Op.LOAD_GLOBAL span reader bf addr 10u extArg
+  | 0x75uy -> parseOperand Op.IS_OP span reader bf addr 2u extArg
+  | 0x76uy -> parseOperand Op.CONTAINS_OP span reader bf addr 2u extArg
+  | 0x77uy -> parseOperand Op.RERAISE span reader bf addr 2u extArg
+  | 0x78uy -> parseOperand Op.COPY span reader bf addr 2u extArg
+  | 0x79uy -> parseOperand Op.RETURN_CONST span reader bf addr 2u extArg
+  | 0x7Auy -> parseOperand Op.BINARY_OP span reader bf addr 4u extArg
+  | 0x7Buy -> parseOperand Op.SEND span reader bf addr 2u extArg
+  | 0x7Cuy -> parseOperand Op.LOAD_FAST span reader bf addr 2u extArg
+  | 0x7Duy -> parseOperand Op.STORE_FAST span reader bf addr 2u extArg
+  | 0x7Euy -> parseOperand Op.DELETE_FAST span reader bf addr 2u extArg
+  | 0x7Fuy -> parseOperand Op.LOAD_FAST_CHECK span reader bf addr 2u extArg
+  | 0x80uy ->
+    parseOperand Op.POP_JUMP_IF_NOT_NONE span reader bf addr 2u extArg
+  | 0x81uy -> parseOperand Op.POP_JUMP_IF_NONE span reader bf addr 2u extArg
+  | 0x82uy -> parseOperand Op.RAISE_VARARGS span reader bf addr 2u extArg
+  | 0x83uy -> parseOperand Op.GET_AWAITABLE span reader bf addr 2u extArg
+  | 0x84uy -> parseOperand Op.MAKE_FUNCTION span reader bf addr 2u extArg
+  | 0x85uy -> parseOperand Op.BUILD_SLICE span reader bf addr 2u extArg
   | 0x86uy ->
-    parseOperand Op.JUMP_BACKWARD_NO_INTERRUPT span reader bFile addr 2u
-  | 0x87uy -> parseOperand Op.MAKE_CELL span reader bFile addr 2u
-  | 0x88uy -> parseOperand Op.LOAD_CLOSURE span reader bFile addr 2u
-  | 0x89uy -> parseOperand Op.LOAD_DEREF span reader bFile addr 2u
-  | 0x8Auy -> parseOperand Op.STORE_DEREF span reader bFile addr 2u
-  | 0x8Buy -> parseOperand Op.DELETE_DEREF span reader bFile addr 2u
-  | 0x8Cuy -> parseOperand Op.JUMP_BACKWARD span reader bFile addr 2u
-  | 0x8Duy -> parseOperand Op.LOAD_SUPER_ATTR span reader bFile addr 2u
-  | 0x8Euy -> parseOperand Op.CALL_FUNCTION_EX span reader bFile addr 2u
-  | 0x8Fuy -> parseOperand Op.LOAD_FAST_AND_CLEAR span reader bFile addr 2u
-  | 0x90uy -> parseOperand Op.EXTENDED_ARG span reader bFile addr 2u
-  | 0x91uy -> parseOperand Op.LIST_APPEND span reader bFile addr 2u
-  | 0x92uy -> parseOperand Op.SET_ADD span reader bFile addr 2u
-  | 0x93uy -> parseOperand Op.MAP_ADD span reader bFile addr 2u
-  | 0x95uy -> parseOperand Op.COPY_FREE_VARS span reader bFile addr 2u
-  | 0x96uy -> parseOperand Op.YIELD_VALUE span reader bFile addr 2u
-  | 0x97uy -> parseOperand Op.RESUME span reader bFile addr 2u
-  | 0x98uy -> parseOperand Op.MATCH_CLASS span reader bFile addr 2u
-  | 0x9Buy -> parseOperand Op.FORMAT_VALUE span reader bFile addr 2u
-  | 0x9Cuy -> parseOperand Op.BUILD_CONST_KEY_MAP span reader bFile addr 2u
-  | 0x9Duy -> parseOperand Op.BUILD_STRING span reader bFile addr 2u
-  | 0xA2uy -> parseOperand Op.LIST_EXTEND span reader bFile addr 2u
-  | 0xA3uy -> parseOperand Op.SET_UPDATE span reader bFile addr 2u
-  | 0xA4uy -> parseOperand Op.DICT_MERGE span reader bFile addr 2u
-  | 0xA5uy -> parseOperand Op.DICT_UPDATE span reader bFile addr 2u
-  | 0xABuy -> parseOperand Op.CALL span reader bFile addr 8u
-  | 0xACuy -> parseOperand Op.KW_NAMES span reader bFile addr 2u
-  | 0xADuy -> parseOperand Op.CALL_INTRINSIC_1 span reader bFile addr 2u
-  | 0xAEuy -> parseOperand Op.CALL_INTRINSIC_2 span reader bFile addr 2u
+    parseOperand Op.JUMP_BACKWARD_NO_INTERRUPT span reader bf addr 2u extArg
+  | 0x87uy -> parseOperand Op.MAKE_CELL span reader bf addr 2u extArg
+  | 0x88uy -> parseOperand Op.LOAD_CLOSURE span reader bf addr 2u extArg
+  | 0x89uy -> parseOperand Op.LOAD_DEREF span reader bf addr 2u extArg
+  | 0x8Auy -> parseOperand Op.STORE_DEREF span reader bf addr 2u extArg
+  | 0x8Buy -> parseOperand Op.DELETE_DEREF span reader bf addr 2u extArg
+  | 0x8Cuy -> parseOperand Op.JUMP_BACKWARD span reader bf addr 2u extArg
+  | 0x8Duy -> parseOperand Op.LOAD_SUPER_ATTR span reader bf addr 2u extArg
+  | 0x8Euy -> parseOperand Op.CALL_FUNCTION_EX span reader bf addr 2u extArg
+  | 0x8Fuy ->
+    parseOperand Op.LOAD_FAST_AND_CLEAR span reader bf addr 2u extArg
+  | 0x91uy -> parseOperand Op.LIST_APPEND span reader bf addr 2u extArg
+  | 0x92uy -> parseOperand Op.SET_ADD span reader bf addr 2u extArg
+  | 0x93uy -> parseOperand Op.MAP_ADD span reader bf addr 2u extArg
+  | 0x95uy -> parseOperand Op.COPY_FREE_VARS span reader bf addr 2u extArg
+  | 0x96uy -> parseOperand Op.YIELD_VALUE span reader bf addr 2u extArg
+  | 0x97uy -> parseOperand Op.RESUME span reader bf addr 2u extArg
+  | 0x98uy -> parseOperand Op.MATCH_CLASS span reader bf addr 2u extArg
+  | 0x9Buy -> parseOperand Op.FORMAT_VALUE span reader bf addr 2u extArg
+  | 0x9Cuy ->
+    parseOperand Op.BUILD_CONST_KEY_MAP span reader bf addr 2u extArg
+  | 0x9Duy -> parseOperand Op.BUILD_STRING span reader bf addr 2u extArg
+  | 0xA2uy -> parseOperand Op.LIST_EXTEND span reader bf addr 2u extArg
+  | 0xA3uy -> parseOperand Op.SET_UPDATE span reader bf addr 2u extArg
+  | 0xA4uy -> parseOperand Op.DICT_MERGE span reader bf addr 2u extArg
+  | 0xA5uy -> parseOperand Op.DICT_UPDATE span reader bf addr 2u extArg
+  | 0xABuy -> parseOperand Op.CALL span reader bf addr 8u extArg
+  | 0xACuy -> parseOperand Op.KW_NAMES span reader bf addr 2u extArg
+  | 0xADuy -> parseOperand Op.CALL_INTRINSIC_1 span reader bf addr 2u extArg
+  | 0xAEuy -> parseOperand Op.CALL_INTRINSIC_2 span reader bf addr 2u extArg
   | 0xAFuy ->
-    parseOperand Op.LOAD_FROM_DICT_OR_GLOBALS span reader bFile addr 2u
-  | 0xB0uy -> parseOperand Op.LOAD_FROM_DICT_OR_DEREF span reader bFile addr 2u
+    parseOperand Op.LOAD_FROM_DICT_OR_GLOBALS span reader bf addr 2u extArg
+  | 0xB0uy ->
+    parseOperand Op.LOAD_FROM_DICT_OR_DEREF span reader bf addr 2u extArg
   | 0xEDuy ->
-    parseOperand Op.INSTRUMENTED_LOAD_SUPER_ATTR span reader bFile addr 2u
+    parseOperand Op.INSTRUMENTED_LOAD_SUPER_ATTR span reader bf addr 2u extArg
   | 0xEEuy ->
-    parseOperand Op.INSTRUMENTED_POP_JUMP_IF_NONE span reader bFile addr 2u
+    parseOperand Op.INSTRUMENTED_POP_JUMP_IF_NONE span reader bf addr 2u extArg
   | 0xEFuy ->
-    parseOperand Op.INSTRUMENTED_POP_JUMP_IF_NOT_NONE span reader bFile addr 2u
-  | 0xF0uy -> parseOperand Op.INSTRUMENTED_RESUME span reader bFile addr 2u
-  | 0xF1uy -> parseOperand Op.INSTRUMENTED_CALL span reader bFile addr 2u
+    parseOperand
+      Op.INSTRUMENTED_POP_JUMP_IF_NOT_NONE span reader bf addr 2u extArg
+  | 0xF0uy -> parseOperand Op.INSTRUMENTED_RESUME span reader bf addr 2u extArg
+  | 0xF1uy -> parseOperand Op.INSTRUMENTED_CALL span reader bf addr 2u extArg
   | 0xF2uy ->
-    parseOperand Op.INSTRUMENTED_RETURN_VALUE span reader bFile addr 2u
-  | 0xF3uy -> parseOperand Op.INSTRUMENTED_YIELD_VALUE span reader bFile addr 2u
+    parseOperand Op.INSTRUMENTED_RETURN_VALUE span reader bf addr 2u extArg
+  | 0xF3uy ->
+    parseOperand Op.INSTRUMENTED_YIELD_VALUE span reader bf addr 2u extArg
   | 0xF4uy ->
-    parseOperand Op.INSTRUMENTED_CALL_FUNCTION_EX span reader bFile addr 2u
+    parseOperand
+      Op.INSTRUMENTED_CALL_FUNCTION_EX span reader bf addr 2u extArg
   | 0xF5uy ->
-    parseOperand Op.INSTRUMENTED_JUMP_FORWARD span reader bFile addr 2u
+    parseOperand Op.INSTRUMENTED_JUMP_FORWARD span reader bf addr 2u extArg
   | 0xF6uy ->
-    parseOperand Op.INSTRUMENTED_JUMP_BACKWARD span reader bFile addr 2u
+    parseOperand Op.INSTRUMENTED_JUMP_BACKWARD span reader bf addr 2u extArg
   | 0xF7uy ->
-    parseOperand Op.INSTRUMENTED_RETURN_CONST span reader bFile addr 2u
-  | 0xF8uy -> parseOperand Op.INSTRUMENTED_FOR_ITER span reader bFile addr 2u
+    parseOperand Op.INSTRUMENTED_RETURN_CONST span reader bf addr 2u extArg
+  | 0xF8uy ->
+    parseOperand Op.INSTRUMENTED_FOR_ITER span reader bf addr 2u extArg
   | 0xF9uy ->
-    parseOperand Op.INSTRUMENTED_POP_JUMP_IF_FALSE span reader bFile addr 2u
+    parseOperand
+      Op.INSTRUMENTED_POP_JUMP_IF_FALSE span reader bf addr 2u extArg
   | 0xFAuy ->
-    parseOperand Op.INSTRUMENTED_POP_JUMP_IF_TRUE span reader bFile addr 2u
-  | 0xFBuy -> parseOperand Op.INSTRUMENTED_END_FOR span reader bFile addr 2u
-  | 0xFCuy -> parseOperand Op.INSTRUMENTED_END_SEND span reader bFile addr 2u
-  | 0xFDuy -> parseOperand Op.INSTRUMENTED_INSTRUCTION span reader bFile addr 2u
-  | 0xFEuy -> parseOperand Op.INSTRUMENTED_LINE span reader bFile addr 2u
-  (*
-  | 0x100uy -> struct (Op.SETUP_FINALLY, NoOperand, 2u)
-  | 0x101uy -> struct (Op.SETUP_CLEANUP, NoOperand, 2u)
-  | 0x102uy -> struct (Op.SETUP_WITH, NoOperand, 2u)
-  | 0x103uy -> struct (Op.POP_BLOCK, NoOperand, 2u)
-  | 0x104uy -> struct (Op.JUMP, NoOperand, 2u)
-  | 0x105uy -> struct (Op.JUMP_NO_INTERRUPT, NoOperand, 2u)
-  | 0x106uy -> struct (Op.LOAD_METHOD, NoOperand, 2u)
-  | 0x107uy -> struct (Op.LOAD_SUPER_METHOD, NoOperand, 2u)
-  | 0x108uy -> struct (Op.LOAD_ZERO_SUPER_METHOD, NoOperand, 2u)
-  | 0x109uy -> struct (Op.LOAD_ZERO_SUPER_ATTR, NoOperand, 2u)
-  | 0x10Auy -> struct (Op.STORE_FAST_MAYBE_NULL, NoOperand, 2u)
-  *)
+    parseOperand
+      Op.INSTRUMENTED_POP_JUMP_IF_TRUE span reader bf addr 2u extArg
+  | 0xFBuy ->
+    parseOperand Op.INSTRUMENTED_END_FOR span reader bf addr 2u extArg
+  | 0xFCuy ->
+    parseOperand Op.INSTRUMENTED_END_SEND span reader bf addr 2u extArg
+  | 0xFDuy ->
+    parseOperand Op.INSTRUMENTED_INSTRUCTION span reader bf addr 2u extArg
+  | 0xFEuy -> parseOperand Op.INSTRUMENTED_LINE span reader bf addr 2u extArg
   | _ -> raise ParsingFailureException
 
+(* Accumulate EXTENDED_ARG prefixes, then parse the real instruction.
+   extArg is already shifted: each EXTENDED_ARG step does (acc ||| n) <<< 8,
+   so the final opcode just ORs its own arg byte into extArg. *)
+let rec private doParse lifter (span: ReadOnlySpan<byte>) (reader: IBinReader)
+                        bf s c e =
+  let op = reader.ReadUInt8(span, 0)
+  let a = reader.ReadUInt8(span, 1) |> int
+  if int op = int Op.EXTENDED_ARG then
+    doParse lifter (span.Slice 2) reader bf s (c + 2UL) ((e ||| a) <<< 8)
+  else
+    let struct (opc, opr, len) = parseInstruction span reader bf c e
+    let total = uint32 (c - s) + len
+    Instruction(s, total, opc, opr, 32<rt>, lifter)
+
 let parse lifter (span: ByteSpan) (reader: IBinReader) binFile addr =
-  let struct (opcode, operands, instrLen) =
-    parseInstruction span reader binFile addr
-  Instruction(addr, instrLen, opcode, operands, 32<rt>, lifter)
+  doParse lifter span reader binFile addr addr 0
